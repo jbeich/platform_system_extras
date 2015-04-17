2fe54e2cc29e0360041ad23771dd03ec6db49336
Than McIntosh <thanm@google.com>
2015-04-17 15:10:43 -0400

    Second round of changes to 'perf' profile collection daemon.
    Details:
    - avoid use of system() in favor of fork/exec.
    - add option to selectively disable/enable mpdecision service around perf
      collection runs to improve profile quality and avoid kernel pmuevents issues.
    - default to using 'simpleperf' instead of 'perf'
    
    Change-Id: I27928d8bb647fd852ec944158ebfd8efa38c01b4

---
+++ perfprofd/cpuconfig.cc
@@ -0,0 +1,113 @@
+/*
+**
+** Copyright 2015, The Android Open Source Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <string>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <cutils/properties.h>
+
+#include "cpuconfig.h"
+#include "perfprofdutils.h"
+
+#define SYSFSCPU "/sys/devices/system/cpu"
+
+HardwireCpuHelper::HardwireCpuHelper(unsigned hardwire, unsigned duration)
+    : ncores_(-1)
+    , mpdecision_running_(GetMpdecisionRunning())
+    , mpdecision_stopped_(false)
+{
+  if (hardwire && duration <= 5 && mpdecision_running_) {
+    mpdecision_stopped_ = true;
+    StopMpdecision();
+    ncores_ = GetNumCores();
+    for (int i = 0; i < ncores_; ++i) {
+      OnlineCore(i, 1);
+    }
+  }
+}
+
+HardwireCpuHelper::~HardwireCpuHelper()
+{
+  if (mpdecision_stopped_) {
+    RestartMpdecision();
+  }
+}
+
+bool HardwireCpuHelper::GetMpdecisionRunning()
+{
+  char propBuf[PROPERTY_VALUE_MAX];
+  propBuf[0] = '\0';
+  property_get("init.svc.mpdecision", propBuf, "");
+  return strcmp(propBuf, "running") == 0;
+}
+
+
+int HardwireCpuHelper::GetNumCores()
+{
+  int ncores = -1;
+  std::string possible(SYSFSCPU "/possible");
+  FILE *fp = fopen(possible.c_str(), "re");
+  if (fp) {
+    char *lineptr = NULL;
+    size_t llen = 0;
+    if (getline(&lineptr, &llen, fp) != -1) {
+      unsigned lo = 0, hi = 0;
+      if (sscanf(lineptr, "%u-%u", &lo, &hi) == 2) {
+        ncores = hi - lo + 1;
+      }
+      free(lineptr);
+    }
+    fclose(fp);
+  }
+  return ncores;
+}
+
+void HardwireCpuHelper::OnlineCore(int i, int onoff)
+{
+  std::stringstream ss;
+  ss << SYSFSCPU "/cpu" << i << "/online";
+  FILE *fp = fopen(ss.str().c_str(), "w");
+  if (fp) {
+    fprintf(fp, onoff ? "1\n" : "0\n");
+    fclose(fp);
+  } else {
+    W_ALOGW("open failed for %s", ss.str().c_str());
+  }
+}
+
+void HardwireCpuHelper::StopMpdecision()
+{
+  if (property_set("ctl.stop", "mpdecision")) {
+    W_ALOGE("setprop ctl.stop mpdecision failed");
+  }
+}
+
+void HardwireCpuHelper::RestartMpdecision()
+{
+  // Don't try to offline the cores we previously onlined -- let
+  // mpdecision figure out what to do
+
+  if (property_set("ctl.start", "mpdecision")) {
+    W_ALOGE("setprop ctl.start mpdecision failed");
+  }
+}
