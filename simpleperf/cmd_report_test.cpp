8c0e9aec76979d963629c923652cc1b63fd445f9
Yabin Cui <yabinc@google.com>
2015-05-21 12:17:23 -0700

    Add simpleperf report command.
    Only support report of process/file level. Symbol parsing is not included.
    
    Bug: 19483574
    Change-Id: I6d1c67000bd0e303623183912ff06fdeeb51c5e9

---
+++ simpleperf/cmd_report_test.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "command.h"
+
+class ReportCommandTest : public ::testing::Test {
+ protected:
+  static void SetUpTestCase() {
+    Command* record_cmd = Command::FindCommandByName("record");
+    ASSERT_TRUE(record_cmd != nullptr);
+    ASSERT_TRUE(record_cmd->Run({"record", "-a", "sleep", "1"}));
+    ASSERT_TRUE(record_cmd->Run({"record", "-a", "-o", "perf2.data", "sleep", "1"}));
+  }
+
+  virtual void SetUp() {
+    report_cmd = Command::FindCommandByName("report");
+    ASSERT_TRUE(report_cmd != nullptr);
+  }
+
+  Command* report_cmd;
+};
+
+TEST_F(ReportCommandTest, no_options) {
+  ASSERT_TRUE(report_cmd->Run({"report"}));
+}
+
+TEST_F(ReportCommandTest, input_file_option) {
+  ASSERT_TRUE(report_cmd->Run({"report", "-i", "perf2.data"}));
+}
+
+TEST_F(ReportCommandTest, exclude_fileinfo_option) {
+  ASSERT_TRUE(report_cmd->Run({"report", "--exclude-fileinfo"}));
+}
---
+++ simpleperf/sample_tree.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SIMPLE_PERF_SAMPLE_TREE_H_
+#define SIMPLE_PERF_SAMPLE_TREE_H_
+
+#include <functional>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "build_id.h"
+
+struct ProcessMap {
+  int pid;
+  uint64_t start_addr;
+  uint64_t len;
+  uint64_t pgoff;
+  uint64_t time;  // Map creation time.
+  std::string filename;
+};
+
+struct Sample {
+  uint64_t ip;
+  uint64_t time;
+  uint64_t cpu;
+  uint64_t period;
+};
+
+struct SampleInMap {
+  int pid;
+  int tid;
+  const ProcessMap* map;
+  std::vector<Sample> samples;
+};
+
+class SampleTree {
+ public:
+  void AddMap(int pid, uint64_t start_addr, uint64_t len, uint64_t pgoff,
+              const std::string& filename, uint64_t time);
+  void AddSample(int pid, int tid, uint64_t ip, uint64_t time, uint32_t cpu, uint64_t period);
+  void VisitAllSamples(std::function<void(const SampleInMap&)> callback) const;
+
+ private:
+  const ProcessMap* FindMap(int pid, uint64_t ip);
+
+  struct ProcessMapComparator {
+    bool operator()(const ProcessMap&, const ProcessMap&);
+  };
+  struct SampleInMapComparator {
+    bool operator()(const SampleInMap&, const SampleInMap&);
+  };
+
+  std::set<ProcessMap, ProcessMapComparator> map_tree_;
+  std::set<SampleInMap, SampleInMapComparator> sample_tree_;
+};
+
+#endif  // SIMPLE_PERF_SAMPLE_TREE_H_
