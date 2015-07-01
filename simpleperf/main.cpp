26d75c18aeb1955760143902ab6b60500b4769b2
Yabin Cui <yabinc@google.com>
2015-06-30 18:03:34 -0700

    Simpleperf: add --log option to ease debugging.
    Bug: 19483574
    Change-Id: If45bbefb79b488206ca7542957f4207c3420fce3

--- simpleperf/main.cpp
+++ simpleperf/main.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <string.h>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -22,9 +23,16 @@
 
 #include "command.h"
 
+static std::map<std::string, char> log_level_map = {
+    {"debug", 'd'},
+    {"warning", 'w'},
+    {"error", 'e'},
+    {"fatal", 'f'},
+};
+
 int main(int argc, char** argv) {
-  InitLogging(argv, android::base::StderrLogger);
   std::vector<std::string> args;
+  char log_level = 'e';
 
   if (argc == 1) {
     args.push_back("help");
@@ -32,11 +40,30 @@ int main(int argc, char** argv) {
     for (int i = 1; i < argc; ++i) {
       if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
         args.insert(args.begin(), "help");
+      } else if (strcmp(argv[i], "--log") == 0) {
+        if (i + 1 < argc) {
+          ++i;
+          auto it = log_level_map.find(argv[i]);
+          if (it != log_level_map.end()) {
+            log_level = it->second;
+          } else {
+            fprintf(stderr, "Unknown log level \"%s\"\n", argv[i]);
+            return 1;
+          }
+        } else {
+          fprintf(stderr, "Missing argument for --log option.\n");
+          return 1;
+        }
       } else {
         args.push_back(argv[i]);
       }
     }
   }
+
+  char log_string[] = "*:d";
+  log_string[2] = log_level;
+  setenv("ANDROID_LOG_TAGS", log_string, 1);
+  InitLogging(argv, android::base::StderrLogger);
 
   std::unique_ptr<Command> command = CreateCommandInstance(args[0]);
   if (command == nullptr) {
