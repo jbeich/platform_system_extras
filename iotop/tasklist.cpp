f400164357d1ab882cc06639ccb62ce5edf7fd1c
Colin Cross <ccross@android.com>
2015-09-03 17:56:39 -0700

    Implement iotop
    iotop provides per process statistics on IO rates and kernel delays.
    Requires root, and a kernel compiled with CONFIG_TASKSTATS,
    CONFIG_TASK_DELAY_ACCT, and CONFIG_TASK_IO_ACCOUNTING.
    
    Change-Id: I111c55a1492e9ea33d8d7b3ab620080625b84346

---
+++ iotop/tasklist.cpp
@@ -0,0 +1,72 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <base/stringprintf.h>
+
+#include "log.h"
+#include "tasklist.h"
+
+bool TaskList::Scan(std::map<pid_t, std::vector<pid_t>>& tgid_map) {
+  tgid_map.clear();
+
+  std::unique_ptr<DIR, decltype(&closedir)> proc_dir(opendir("/proc"), closedir);
+  if (!proc_dir) {
+    return false;
+  }
+
+  dirent* entry;
+  while ((entry = readdir(proc_dir.get())) != nullptr) {
+    if (isdigit(entry->d_name[0])) {
+      pid_t tgid = atoi(entry->d_name);
+
+      std::vector<pid_t> pid_list;
+      if (!ScanPid(tgid, pid_list)) {
+        continue;
+      }
+      tgid_map.insert(std::make_pair(tgid, pid_list));
+    }
+  }
+
+  return true;
+}
+
+bool TaskList::ScanPid(pid_t tgid, std::vector<pid_t>& pid_list) {
+  std::string filename = android::base::StringPrintf("/proc/%d/task", tgid);
+
+  std::unique_ptr<DIR, decltype(&closedir)> task_dir(opendir(filename.c_str()), closedir);
+  if (!task_dir) {
+    return false;
+  }
+
+  dirent* entry;
+  while ((entry = readdir(task_dir.get())) != nullptr) {
+    if (isdigit(entry->d_name[0])) {
+      pid_t pid = atoi(entry->d_name);
+      pid_list.push_back(pid);
+    }
+  }
+
+  return true;
+}
