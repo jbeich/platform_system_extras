a94fd000a1f4edacf892d207aa94cf69569cfda3
liping.zhang <liping.zhang@spreadtrum.com>
2015-11-09 15:45:30 +0800

    WIP: tcp_nuke_addr accept crash test
    1. fix a file descriptor leak bug
    2. do not kill netd (I will find the reason why netd
       occupy high cpu when do tcp_nuke_addr_test later)
    
    Change-Id: Iec3da69861875e06ce46e16e526129a611e1add3

--- tests/tcp_nuke_addr/Android.mk
+++ tests/tcp_nuke_addr/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  tcp_nuke_addr_test.c
+
 LOCAL_MODULE := tcp_nuke_addr_test
 
-LOCAL_C_INCLUDES += frameworks/native/include external/libcxx/include
-LOCAL_CPPFLAGS += -std=c++11 -Wall -Werror
-LOCAL_SHARED_LIBRARIES := libc++
-LOCAL_SRC_FILES := tcp_nuke_addr_test.cpp
-LOCAL_MODULE_TAGS := eng tests
+LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -DNO_SCRIPT
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_SHARED_LIBRARIES := libnetutils liblog
 
-include $(BUILD_NATIVE_TEST)
+include $(BUILD_EXECUTABLE)
