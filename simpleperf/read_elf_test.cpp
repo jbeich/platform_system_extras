95538ca34b8679358ceca5220434ee6c46638e93
Yabin Cui <yabinc@google.com>
2015-12-08 17:43:15 -0800

    Simpleperf: check if elf file paths are valid before reading them.
    Reading invalid file paths can cause troubles. For example,
    reading /dev/zero can eat all memory.
    
    Bug: 25194400
    Change-Id: I4de17f4f9200a43b50a4efe4c42a6fd9eaec1ba6

--- simpleperf/read_elf_test.cpp
+++ simpleperf/read_elf_test.cpp
@@ -24,13 +24,18 @@ static void ParseSymbol(const ElfFileSymbol& symbol, bool* result) {
   }
 }
 
-TEST(read_elf, parse_symbols_from_elf_file) {
+static void ReadLink(const std::string& from, std::string* to) {
   char elf_file[PATH_MAX];
-  ssize_t elf_file_len = readlink("/proc/self/exe", elf_file, sizeof(elf_file));
+  ssize_t elf_file_len = readlink(from.c_str(), elf_file, sizeof(elf_file));
   ASSERT_GT(elf_file_len, 0L);
   ASSERT_LT(static_cast<size_t>(elf_file_len), sizeof(elf_file));
   elf_file[elf_file_len] = '\0';
+  *to = elf_file;
+}
 
+TEST(read_elf, parse_symbols_from_elf_file) {
+  std::string elf_file;
+  ReadLink("/proc/self/exe", &elf_file);
   BuildId build_id;
   GetBuildIdFromElfFile(elf_file, &build_id);
   bool result = false;
@@ -44,4 +49,13 @@ TEST(read_elf, arm_mapping_symbol) {
   ASSERT_FALSE(IsArmMappingSymbol("$b"));
   ASSERT_TRUE(IsArmMappingSymbol("$a.anything"));
   ASSERT_FALSE(IsArmMappingSymbol("$a_no_dot"));
+}
+
+TEST(read_elf, IsValidElfPath) {
+  ASSERT_FALSE(IsValidElfPath("/dev/zero"));
+  ASSERT_FALSE(IsValidElfPath("/sys/devices/system/cpu/online"));
+  ASSERT_FALSE(IsValidElfPath("/proc/self/exe"));
+  std::string elf_file;
+  ReadLink("/proc/self/exe", &elf_file);
+  ASSERT_TRUE(IsValidElfPath(elf_file));
 }
