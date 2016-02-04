ed587a4151a0025b097c54885f84343bd3620354
Chris Phoenix <cphoenix@google.com>
2016-02-03 18:01:45 -0800

    First commit of fdt_overlay code and test scripts.
    Change-Id: I770aa06986c677da21e02c9b87da1708e2f7ebe2

---
+++ libdtoverlay/Android.mk
@@ -0,0 +1,20 @@
+# Copyright 2016 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := fdt_overlay.c
+LOCAL_STATIC_LIBRARIES := libfdt
+LOCAL_MODULE := libdtoverlay
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := dtoverlay_test_app
+LOCAL_SRC_FILES := dtoverlay_test_app.c
+LOCAL_STATIC_LIBRARIES := libdtoverlay libfdt
+LOCAL_REQUIRED_MODULES := dtc
+include $(BUILD_HOST_NATIVE_TEST)
+
+
