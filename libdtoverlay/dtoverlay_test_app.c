385f97bbaca72d5ade3288b7252c588ff13e3dce
Chris Phoenix <cphoenix@google.com>
2016-02-03 18:01:45 -0800

    Modify fdt_overlay code and add test scripts.
    BUG=31386003
    
    Change-Id: I770aa06986c677da21e02c9b87da1708e2f7ebe2

---
+++ libdtoverlay/dtoverlay_test_app.c
@@ -0,0 +1,55 @@
+#include "fdt_overlay.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *load_file(char *fname, size_t *pLen);
+
+char *load_file(char *fname, size_t *pLen) {
+  FILE *f;
+  f = fopen(fname, "r");
+  if (!f) {
+    printf("Couldn't open file '%s'\n", fname);
+    exit(1);
+  }
+  fseek(f, 0, SEEK_END);
+  *pLen = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  char *buf = malloc(*pLen);
+  if (fread(buf, *pLen, 1, f) != 1) {
+    printf("Bad fread");
+    exit(1);
+  }
+  return buf;
+}
+
+int main(int argc, char **argv) {
+  char *base_buf, *overlay_buf;
+  FILE *out_file;
+  struct fdt_header *blob;
+  if (argc < 4) {
+    printf("Usage: ov_test base_file overlay_file out_file\n");
+    exit(1);
+  }
+  size_t blob_len, overlay_len;
+  base_buf = load_file(argv[1], &blob_len);
+  overlay_buf = load_file(argv[2], &overlay_len);
+  if (!overlay_buf) return 1;
+  blob = fdt_install_blob(base_buf, blob_len);
+  if (!blob) {
+    printf("fdt_install_blob returned null\n");
+    exit(1);
+  }
+  struct fdt_header *new_blob = apply_overlay(blob, blob_len, overlay_buf, overlay_len);
+  // Do not free(blob) - it's the same as base_buf.
+
+  out_file = fopen(argv[3], "wb");
+  if (fwrite(new_blob, 1, fdt_totalsize(new_blob), out_file) < 1) {
+    printf("fwrite failed\n");
+    exit(1);
+  }
+  free(new_blob);
+  free(base_buf);
+  free(overlay_buf);
+  return 0;
+}
