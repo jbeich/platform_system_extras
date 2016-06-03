9a02a0de841c7fe43f730ba7e672d260de405e95
Yabin Cui <yabinc@google.com>
2016-06-03 15:06:53 -0700

    simpleperf: support report samples using format defined by protobuf.
    Bug: 28114205
    Change-Id: Idf6d64b8418b3d55d5a43f6096dd2d69764692c0

--- simpleperf/cmd_report_sample.cpp
+++ simpleperf/cmd_report_sample.cpp
@@ -18,12 +18,31 @@
 
 #include <memory>
 
+#include "system/extras/simpleperf/report_sample.pb.h"
+
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+
 #include "command.h"
 #include "record_file.h"
 #include "thread_tree.h"
 #include "utils.h"
 
+namespace proto = simpleperf_report_proto;
+
 namespace {
+
+class ProtobufFileWriter : public google::protobuf::io::CopyingOutputStream {
+ public:
+  ProtobufFileWriter(FILE* out_fp) : out_fp_(out_fp) {}
+
+  bool Write(const void* buffer, int size) override {
+    return fwrite(buffer, size, 1, out_fp_) == 1;
+  }
+
+ private:
+  FILE* out_fp_;
+};
 
 class ReportSampleCommand : public Command {
  public:
@@ -34,12 +53,16 @@ class ReportSampleCommand : public Command {
 "Usage: simpleperf report-sample [options]\n"
 "-i <file>  Specify path of record file, default is perf.data.\n"
 "-o report_file_name  Set report file name, default is stdout.\n"
+"--protobuf  Use protobuf format in report_sample.proto to output samples.\n"
+"            Need to set a report_file_name when using this option.\n"
 "--show-callchain  Print callchain samples.\n"
             // clang-format on
-
             ),
         record_filename_("perf.data"),
         show_callchain_(false),
+        use_protobuf_(false),
+        report_fp_(nullptr),
+        coded_os_(nullptr),
         sample_count_(0) {}
 
   bool Run(const std::vector<std::string>& args) override;
@@ -53,9 +76,11 @@ class ReportSampleCommand : public Command {
   std::string record_filename_;
   std::unique_ptr<RecordFileReader> record_file_reader_;
   bool show_callchain_;
+  bool use_protobuf_;
   ThreadTree thread_tree_;
   std::string report_filename_;
   FILE* report_fp_;
+  google::protobuf::io::CodedOutputStream* coded_os_;
   size_t sample_count_;
 };
 
@@ -64,6 +89,10 @@ bool ReportSampleCommand::Run(const std::vector<std::string>& args) {
   if (!ParseOptions(args)) {
     return false;
   }
+  if (use_protobuf_) {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+  }
+
   // 2. Open record file.
   record_file_reader_ = RecordFileReader::CreateInstance(record_filename_);
   if (record_file_reader_ == nullptr) {
@@ -73,13 +102,24 @@ bool ReportSampleCommand::Run(const std::vector<std::string>& args) {
   // 3. Prepare report output stream.
   report_fp_ = stdout;
   std::unique_ptr<FILE, decltype(&fclose)> fp(nullptr, fclose);
+  std::unique_ptr<ProtobufFileWriter> protobuf_writer;
+  std::unique_ptr<google::protobuf::io::CopyingOutputStreamAdaptor> protobuf_os;
+  std::unique_ptr<google::protobuf::io::CodedOutputStream> protobuf_coded_os;
   if (!report_filename_.empty()) {
-    fp.reset(fopen(report_filename_.c_str(), "w"));
+    fp.reset(fopen(report_filename_.c_str(), use_protobuf_ ? "wb" : "w"));
     if (fp == nullptr) {
       PLOG(ERROR) << "failed to open " << report_filename_;
       return false;
     }
     report_fp_ = fp.get();
+  }
+  if (use_protobuf_) {
+    protobuf_writer.reset(new ProtobufFileWriter(report_fp_));
+    protobuf_os.reset(new google::protobuf::io::CopyingOutputStreamAdaptor(
+        protobuf_writer.get()));
+    protobuf_coded_os.reset(
+        new google::protobuf::io::CodedOutputStream(protobuf_os.get()));
+    coded_os_ = protobuf_coded_os.get();
   }
 
   // 4. Read record file, and print samples online.
@@ -91,7 +131,17 @@ bool ReportSampleCommand::Run(const std::vector<std::string>& args) {
   }
   LOG(INFO) << "report " << sample_count_ << " samples in all.";
 
-  fflush(report_fp_);
+  if (use_protobuf_) {
+    coded_os_->WriteLittleEndian32(0);
+    if (coded_os_->HadError()) {
+      LOG(ERROR) << "print protobuf report failed";
+      return false;
+    }
+    protobuf_coded_os.reset(nullptr);
+    google::protobuf::ShutdownProtobufLibrary();
+  } else {
+    fflush(report_fp_);
+  }
   if (ferror(report_fp_) != 0) {
     PLOG(ERROR) << "print report failed";
     return false;
@@ -111,12 +161,19 @@ bool ReportSampleCommand::ParseOptions(const std::vector<std::string>& args) {
         return false;
       }
       report_filename_ = args[i];
+    } else if (args[i] == "--protobuf") {
+      use_protobuf_ = true;
     } else if (args[i] == "--show-callchain") {
       show_callchain_ = true;
     } else {
       ReportUnknownOption(args, i);
       return false;
     }
+  }
+
+  if (use_protobuf_ && report_filename_.empty()) {
+    LOG(ERROR) << "please specify a report filename to write protobuf data";
+    return false;
   }
   return true;
 }
@@ -125,7 +182,69 @@ bool ReportSampleCommand::ProcessRecord(std::unique_ptr<Record> record) {
   thread_tree_.Update(*record);
   if (record->type() == PERF_RECORD_SAMPLE) {
     sample_count_++;
-    return PrintSampleRecord(*static_cast<const SampleRecord*>(record.get()));
+    auto& r = *static_cast<const SampleRecord*>(record.get());
+    if (use_protobuf_) {
+      return PrintSampleRecordInProtobuf(r);
+    } else {
+      return PrintSampleRecord(r);
+    }
+  }
+  return true;
+}
+
+bool ReportSampleCommand::PrintSampleRecordInProtobuf(const SampleRecord& r) {
+  proto::Record proto_record;
+  proto_record.set_type(proto::Record_Type_SAMPLE);
+  proto::Sample* sample = proto_record.mutable_sample();
+  sample->set_time(r.time_data.time);
+  proto::Sample_CallChainEntry* callchain = sample->add_callchain();
+  callchain->set_ip(r.ip_data.ip);
+
+  bool in_kernel = r.InKernel();
+  const ThreadEntry* thread =
+      thread_tree_.FindThreadOrNew(r.tid_data.pid, r.tid_data.tid);
+  const MapEntry* map = thread_tree_.FindMap(thread, r.ip_data.ip, in_kernel);
+  const Symbol* symbol = thread_tree_.FindSymbol(map, r.ip_data.ip);
+  callchain->set_symbol(symbol->DemangledName());
+  callchain->set_file(map->dso->Path());
+
+  if (show_callchain_) {
+    const std::vector<uint64_t>& ips = r.callchain_data.ips;
+    bool first_ip = true;
+    for (auto& ip : ips) {
+      if (ip >= PERF_CONTEXT_MAX) {
+        switch (ip) {
+          case PERF_CONTEXT_KERNEL:
+            in_kernel = true;
+            break;
+          case PERF_CONTEXT_USER:
+            in_kernel = false;
+            break;
+          default:
+            LOG(DEBUG) << "Unexpected perf_context in callchain: " << std::hex
+                       << ip << std::dec;
+        }
+      } else {
+        if (first_ip) {
+          first_ip = false;
+          // Remove duplication with sample ip.
+          if (ip == r.ip_data.ip) {
+            continue;
+          }
+        }
+        const MapEntry* map = thread_tree_.FindMap(thread, ip, in_kernel);
+        const Symbol* symbol = thread_tree_.FindSymbol(map, ip);
+        callchain = sample->add_callchain();
+        callchain->set_ip(ip);
+        callchain->set_symbol(symbol->DemangledName());
+        callchain->set_file(map->dso->Path());
+      }
+    }
+  }
+  coded_os_->WriteLittleEndian32(proto_record.ByteSize());
+  if (!proto_record.SerializeToCodedStream(coded_os_)) {
+    LOG(ERROR) << "failed to write sample to protobuf";
+    return false;
   }
   return true;
 }
