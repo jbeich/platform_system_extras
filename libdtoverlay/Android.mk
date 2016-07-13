17d7f4b1b8f322f4f659af16092d2fd35e07f030
LiChen <akaineko@google.com>
2016-07-13 11:55:39 +0800

    Increase test coverage of libdtoverlay.
    Adding more test cases to test libdtoverlay.
    Note that it currently fails in several test cases because
    of some issues in current version of libdtoverlay (see details below).
    
    The following section lists different test cases and its purpose.
    
    - apply_fragment.*:
      It's to test about overlaying fragments into new flattened device tree.
      Current version of libdtoverlay fails on this test because it doesn't update
      phandle value of nodes if there's nothing in __local_fixups__. So there're
      duplicates in phandle after overlay some fdts.
    
    - dtc_local_fixup.*:
    - libdtoverlay_local_fixup.*:
      Both are to test about dealing with __local_fixups__ (especially about update
      of local phandle). Current version of libdtoverlay fails on the above two tests
      because the format of __local_fixups__ generated by current dtc(`external/dtc`)
      doesn't match the format expected by libdtoverlay. It's because `external/dtc`
      is updated with some change in the format of __local_fixups__. But
      current version of libdtoverlay doesn't follow this change.
    
    - no_local_fixup.*:
      Renamed from previous `a.*` test case which is to test about basic fragment overlay.
    
    - local_fixup_with_offset.*:
      It's to test about local fixups look like
      `a = <0 &p 2>;` which is referenced inside an array.
    
    Some modifications in `tests/gen_test.sh`.
    Now it allows developers to add their own overlay files under ./tests/testdata for
    testing.
    
    Change-Id: I3bd3f1febf850488757505635a40a0d9e94fa9eb

--- libdtoverlay/Android.mk
+++ libdtoverlay/Android.mk
@@ -2,19 +2,23 @@
 
 LOCAL_PATH:= $(call my-dir)
 
+###################################################
 
 include $(CLEAR_VARS)
+
+LOCAL_MODULE := libdtoverlay
 LOCAL_SRC_FILES := fdt_overlay.c
 LOCAL_STATIC_LIBRARIES := libfdt
-LOCAL_MODULE := libdtoverlay
+
 include $(BUILD_HOST_STATIC_LIBRARY)
 
+####################################################
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := dtoverlay_test_app
 LOCAL_SRC_FILES := dtoverlay_test_app.c
 LOCAL_STATIC_LIBRARIES := libdtoverlay libfdt
 LOCAL_REQUIRED_MODULES := dtc
+
 include $(BUILD_HOST_NATIVE_TEST)
-
-
