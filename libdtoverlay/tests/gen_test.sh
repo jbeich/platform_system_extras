ecb6ee33ec71c569b5f397f5e6c2fde6a65ae7b3
LiChen <akaineko@google.com>
2016-07-13 11:55:39 +0800

    Increase test coverage of libdtoverlay.
    Adding more test cases to test libdtoverlay.
    Note that it currently fails in several test cases because
    of some issues in current version of libdtoverlay (see details below).
    
    The following section lists different test cases and its purpose.
    
    - apply_fragment.*:
      It's to test about overlaying fragments into new flattened device tree.
      Current version of libdtoverlay fails on this test because it doesn't update
      phandle value of nodes if there's nothing in __local_fixups__. So there're
      duplicates in phandle after overlay some fdts.
    
    - dtc_local_fixup.*:
    - libdtoverlay_local_fixup.*:
      Both are to test about dealing with __local_fixups__ (especially about update
      of local phandle). Current version of libdtoverlay fails on the above two tests
      because the format of __local_fixups__ generated by current dtc(`external/dtc`)
      doesn't match the format expected by libdtoverlay. It's because `external/dtc`
      is updated with some change in the format of __local_fixups__. But
      current version of libdtoverlay doesn't follow this change.
    
    - no_local_fixup.*:
      Renamed from previous `a.*` test case which is to test about basic fragment overlay.
    
    - local_fixup_with_offset.*:
      It's to test about local fixups look like
      `a = <0 &p 2>;` which is referenced inside an array.
    
    Some modifications in `tests/gen_test.sh`.
    Now it allows developers to add their own overlay files under ./tests/testdata for
    testing.
    
    Change-Id: I3bd3f1febf850488757505635a40a0d9e94fa9eb

--- libdtoverlay/tests/gen_test.sh
+++ libdtoverlay/tests/gen_test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-fname=$1
-
-# We want to generate two device tree blob (.dtb) files by combining 
-# the "base" and "add" device tree source (.dts) files in two 
+# We want to generate two device tree blob (.dtb) files by combining
+# the "base" and "add" device tree source (.dts) files in two
 # different ways.
 #
 # 1) /include/ the "add" at the end of the "base" file and
@@ -15,65 +13,80 @@ fname=$1
 #
 # To do this, we need to generate a lot of files from the .base_dts
 #    and .add_dts files:
-# .base_inc_dts - Has the /include/ "$fname.add_dts" at the end. 
+# .base_inc_dts - Has the /include/ "${FILENAME}.add_dts" at the end.
 # .base_inc_dtb - The dtc-generated "gold standard"
 # .add_ov_dts - Has the /plugin/ at the start
 # .base_dtb - Compiled version of just the base
 # .add_ov_dtbo - Compiled version of the overlay
 # .base_ov_dtb - overlay-test-joined version of the dtb
 #
-# Then, compare .base_inc_dtb and .base_ov_dtb with dtdiff 
+# Then, compare .base_inc_dtb and .base_ov_dtb with dtdiff
 # (or maybe diff?) and return 0 iff they are identical.
 
-#set -x
+# Include some functions from common.sh.
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+source ${SCRIPT_DIR}/common.sh
 
-test_gen=$OUT/gen/test
-mkdir $test_gen >& /dev/null
+# Constants
+IN_DATA_DIR="testdata"
+OUT_DATA_DIR="test-out"
 
-cp "$fname.base_dts" "$test_gen/$fname.base_dts"
-cp "$fname.add_dts" "$test_gen/$fname.add_dts"
+# Global variables
+FILENAME=$1
 
-# Add the "include" to make .base_inc_dts
-cp "$fname.base_dts" "$test_gen/$fname.base_inc_dts"
-echo "/include/ \"$fname.add_dts\"" >> "$test_gen/$fname.base_inc_dts"
+on_exit() {
+  rm -rf "${OUT_DATA_DIR}"
+}
 
-# Generate .base_inc_dtb
-dtc -O dtb -b 0 -o "$test_gen/$fname.base_inc.dtb" \
-  "$test_gen/$fname.base_inc_dts"
+# Generate test cases to OUT_DATA_DIR
+prepare_tests() {
+  cp "${IN_DATA_DIR}/${FILENAME}.base_dts" "${OUT_DATA_DIR}/${FILENAME}.base_dts"
+  cp "${IN_DATA_DIR}/${FILENAME}.add_dts" "${OUT_DATA_DIR}/${FILENAME}.add_dts"
 
-#/ {
-#	fragment@0 {
-#		target = <&i2c_3>;
-#		__overlay__ {
-# Prepend the "plugin" to make .add_ov_dts
-sed "1{N;N;s/\/ *\([{]\)[ \t\n\r]*\([^ \t\n\r]\{1,\}\)/\/dts-v1\/ \/plugin\/;\n\/ \1\n fragment@0 \1\n target = <\&\2>;\n __overlay__ /}" "$fname.add_dts" > "$test_gen/$fname.add_ov_dts"
-echo "};" >> "$test_gen/$fname.add_ov_dts"
+  # Add the "include" to make .base_inc_dts
+  cp "${IN_DATA_DIR}/${FILENAME}.base_dts" "${OUT_DATA_DIR}/${FILENAME}.base_inc_dts"
+  echo "/include/ \"${FILENAME}.add_dts\"" >> "${OUT_DATA_DIR}/${FILENAME}.base_inc_dts"
 
-# Make a standalone of the "add" to help the user find syntax errors
-echo "/dts-v1/;" > "$test_gen/$fname.add_sa_dts"
-cat "$fname.add_dts" >> "$test_gen/$fname.add_sa_dts"
+  # Generate .base_inc_dtb
+  dtc -@ -O dtb -s -b 0 -o "${OUT_DATA_DIR}/${FILENAME}.base_inc.dtb" \
+    "${OUT_DATA_DIR}/${FILENAME}.base_inc_dts"
 
-# Compile the base to make .base_dtb
-dtc -O dtb -b 0 -@ -o "$test_gen/$fname.base_dtb" \
-  "$test_gen/$fname.base_dts"
+  # Add /dts-v1/ /plugin/; in front of .add_dts file. In order to trigger dtc's
+  # fragment generation mechanism.
+  if [ -a "${IN_DATA_DIR}/${FILENAME}.add_ov_dts" ]; then
+    cp "${IN_DATA_DIR}/${FILENAME}.add_ov_dts" "${OUT_DATA_DIR}/${FILENAME}.add_ov_dts"
+  else
+    sed "1i/dts-v1/ /plugin/;" "${IN_DATA_DIR}/${FILENAME}.add_dts" > \
+      "${OUT_DATA_DIR}/${FILENAME}.add_ov_dts"
+  fi
+}
 
-# Compile the standalone of the add (syntax error checking)
-dtc -O dtb -b 0 -@ -o "$test_gen/$fname.add_sa_dtb" \
-  "$test_gen/$fname.add_sa_dts"
+# Compile test cases into dtb, and do the diff things.
+compile_and_diff() {
+  # Compile the base to make .base_dtb
+  dtc -O dtb -b 0 -@ -o "${OUT_DATA_DIR}/${FILENAME}.base_dtb" \
+    "${OUT_DATA_DIR}/${FILENAME}.base_dts"
 
-# Compile the overlay to make .add_ov_dtbo
-dtc -O dtb -b 0 -@ -o "$test_gen/$fname.add_ov_dtbo" \
-  "$test_gen/$fname.add_ov_dts"
+  # Compile the overlay to make .add_ov_dtbo
+  dtc -O dtb -b 0 -@ -o "${OUT_DATA_DIR}/${FILENAME}.add_ov_dtbo" \
+    "${OUT_DATA_DIR}/${FILENAME}.add_ov_dts"
 
-# Run ov_test to combine .base_dtb and .add_ov_dtbo
-#   into .base_ov_dtb
-dtoverlay_test_app "$test_gen/$fname.base_dtb" \
-  "$test_gen/$fname.add_ov_dtbo" \
-  "$test_gen/$fname.base_ov.dtb"
+  # Run ov_test to combine .base_dtb and .add_ov_dtbo
+  # into .base_ov_dtb
+  dtoverlay_test_app "${OUT_DATA_DIR}/${FILENAME}.base_dtb" \
+    "${OUT_DATA_DIR}/${FILENAME}.add_ov_dtbo" \
+    "${OUT_DATA_DIR}/${FILENAME}.base_ov.dtb"
 
-source base.sh
+  # Run the diff
+  dt_diff ${OUT_DATA_DIR}/${FILENAME}.base_inc.dtb ${OUT_DATA_DIR}/${FILENAME}.base_ov.dtb
+}
 
-# Run the diff
-dt_diff $test_gen/$fname.base_inc.dtb $test_gen/$fname.base_ov.dtb
+# The script will exit directly if any command fails.
+set -e
+trap on_exit EXIT
 
-set +x
+mkdir -p "${OUT_DATA_DIR}" >& /dev/null
+
+prepare_tests
+compile_and_diff
+
