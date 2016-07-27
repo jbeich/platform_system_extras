f1e369f4c3633c956a069fa086afc0975c79e962
David Zeuthen <zeuthen@google.com>
2016-07-27 17:58:40 -0400

    avb: Add common verified boot tools and library.
    This code is originally from the Brillo project but has been adapted for
    use in all of Android. It consists of a tool - avbtool - for working
    with images (e.g. boot.img, system.img). See the README file for how
    it's integrated into the Android build system and how to enable it.
    
    The main job of avbtool is to create vbmeta.img which is the
    top-level object for verified boot. This image is designed to go into
    the vbmeta partition (or, if using A/B, the slot in question
    e.g. vbmeta_a or vbmeta_b) and be of minimal size (for out-of-band
    updates). The vbmeta image is cryptographically signed and contains
    verification data (e.g. cryptographic digests) for verifying boot.img,
    system.img, and other partitions/images.
    
    The vbmeta image can also contain references to other partitions where
    verification data is stored as well as a public key indicating who
    should sign the verification data. This indirection provides
    delegation, that is, it allows a 3rd party to control content on a given
    partition by including the public key said 3rd party is using to sign
    the data with, in vbmeta.img. By design, this authority can be easily
    revoked by simply updating vbmeta.img with new descriptors for the
    partition in question.
    
    Storing signed verification data on other images - for example
    boot.img and system.img - is also done with avbtool.
    
    In addition to avbtool, a library - libavb - is provided. This library
    performs all verification on the device side e.g. it starts by loading
    the vbmeta partition, checks the signature, and then goes on to load
    the boot partition for verification.
    
    The libavb library is intended to be used in both boot loaders and
    inside Android. It has a simple abstraction for system dependencies
    (see libavb/avb_sysdeps.h) as well as operations that the boot loader
    or OS is expected to implement (see libavb/avb_ops.h).
    
    In addition to handling verified boot, libavb will in the future be
    extended to handle A/B selection in a way that can be used in the
    device's fastboot implementation, its boot loader, and its
    boot_control HAL implementation. This will be implemented in a future
    CL.
    
    BUG=29414516
    TEST=Unit tests for avbtool and libavb + unit tests pass.
    
    Change-Id: I69ee86878e21fa718faccfc56eb0b1f40707d847

---
+++ avb/libavb/avb_kernel_cmdline_descriptor.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "avb_kernel_cmdline_descriptor.h"
+#include "avb_util.h"
+
+int avb_kernel_cmdline_descriptor_validate_and_byteswap(
+    const AvbKernelCmdlineDescriptor* src, AvbKernelCmdlineDescriptor* dest) {
+  uint64_t expected_size;
+
+  avb_memcpy(dest, src, sizeof(AvbKernelCmdlineDescriptor));
+
+  if (!avb_descriptor_validate_and_byteswap((const AvbDescriptor*)src,
+                                            (AvbDescriptor*)dest))
+    return 0;
+
+  if (dest->parent_descriptor.tag != AVB_DESCRIPTOR_TAG_KERNEL_CMDLINE) {
+    avb_warning("Invalid tag %" PRIu64 " for kernel cmdline descriptor.\n",
+                dest->parent_descriptor.tag);
+    return 0;
+  }
+
+  dest->kernel_cmdline_length = avb_be32toh(dest->kernel_cmdline_length);
+
+  // Check that kernel_cmdline is fully contained.
+  expected_size = sizeof(AvbKernelCmdlineDescriptor) - sizeof(AvbDescriptor);
+  if (!avb_safe_add_to(&expected_size, dest->kernel_cmdline_length)) {
+    avb_warning("Overflow while adding up sizes.\n");
+    return 0;
+  }
+  if (expected_size > dest->parent_descriptor.num_bytes_following) {
+    avb_warning("Descriptor payload size overflow.\n");
+    return 0;
+  }
+
+  return 1;
+}
