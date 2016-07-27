474b73d92292a0fab04055fd43fd8aaf614273fc
David Zeuthen <zeuthen@google.com>
2016-07-27 17:58:40 -0400

    avb: Add common verified boot tools and library.
    This code is originally from the Brillo project but has been adapted for
    use in all of Android. It consists of a tool - abvtool - for working
    with images (e.g. boot.img, system.img). See the README file for how
    it's integrated into the Android build system and how to enable it.
    
    The main job of avbtool is for creating vbmeta.img which is the
    top-level object for verified boot. This image is designed to go into
    the vbmeta partition (or, if using A/B, the slot in question
    e.g. vbmeta_a or vbmeta_b) and be of minimal size (for out-of-band
    updates). The vbmeta image is cryptographically signed and contain
    verification data (e.g. cryptographic digests) for verifying boot.img,
    system.img, and other partitions/images.
    
    The vbmeta image can also contain references to other partitions where
    verification data is stored as well as a public key indicating who
    should sign the verification data. This indirection provides
    delegation, that is, it allows 3rd party to control content on a given
    partition by including the public key said 3rd party is using to sign
    the data with, in vbmeta.img. By design, this authority can be easily
    revoked by simply updating vbmeta.img with new descriptors for the
    partition in question.
    
    Storing signed verification data on other images - for example
    boot.img and system.img - is also done with avbtool.
    
    In addition to avbtool, a library - libavb - is provided. This library
    performs all verification on the device side e.g. it starts by loading
    the vbmeta partition, checks the signature, and then goes on to load
    the boot partition for verification.
    
    The libavb library is intended to be used in both boot loaders and
    inside Android. It has a simple abstraction for system dependencies
    (see libavb/avb_sysdeps.h) as well as operations that the boot loader
    or OS is expected to implement (see libavb/avb_ops.h).
    
    In addition to handling verified boot, libavb will in the future be
    extended to handle A/B selection in a way that can be used in the
    device's fastboot implementation, its boot loader, and its
    boot_control HAL implementation. This will be implemented in a future
    CL.
    
    BUG=29414516
    TEST=Unit tests for avbtool and libavb + unit tests pass.
    
    Change-Id: I69ee86878e21fa718faccfc56eb0b1f40707d847

---
+++ avb/libavb/avb_property_descriptor.c
@@ -0,0 +1,180 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "avb_property_descriptor.h"
+#include "avb_util.h"
+
+int avb_property_descriptor_validate_and_byteswap(
+    const AvbPropertyDescriptor* src, AvbPropertyDescriptor* dest) {
+  uint64_t expected_size;
+
+  avb_memcpy(dest, src, sizeof(AvbPropertyDescriptor));
+
+  if (!avb_descriptor_validate_and_byteswap((const AvbDescriptor*)src,
+                                            (AvbDescriptor*)dest))
+    return 0;
+
+  if (dest->parent_descriptor.tag != AVB_DESCRIPTOR_TAG_PROPERTY) {
+    avb_warning("Invalid tag %" PRIu64 " for property descriptor.\n",
+                dest->parent_descriptor.tag);
+    return 0;
+  }
+
+  dest->key_num_bytes = avb_be64toh(dest->key_num_bytes);
+  dest->value_num_bytes = avb_be64toh(dest->value_num_bytes);
+
+  // Check that key and value are fully contained.
+  expected_size = sizeof(AvbPropertyDescriptor) - sizeof(AvbDescriptor) + 2;
+  if (!avb_safe_add_to(&expected_size, dest->key_num_bytes) ||
+      !avb_safe_add_to(&expected_size, dest->value_num_bytes)) {
+    avb_warning("Overflow while adding up sizes.\n");
+    return 0;
+  }
+  if (expected_size > dest->parent_descriptor.num_bytes_following) {
+    avb_warning("Descriptor payload size overflow.\n");
+    return 0;
+  }
+
+  return 1;
+}
+
+typedef struct {
+  const char* key;
+  size_t key_size;
+  const char* ret_value;
+  size_t ret_value_size;
+} PropertyIteratorData;
+
+static int property_lookup_desc_foreach(const AvbDescriptor* header,
+                                        void* user_data) {
+  PropertyIteratorData* data = (PropertyIteratorData*)user_data;
+  const uint8_t* p;
+  const AvbPropertyDescriptor* ph;
+  uint64_t key_nb;
+  uint64_t value_nb;
+  int ret = 1;
+
+  if (header->tag != AVB_DESCRIPTOR_TAG_PROPERTY) goto out;
+
+  p = (const uint8_t*)header;
+  ph = (const AvbPropertyDescriptor*)header;
+  key_nb = avb_be64toh(ph->key_num_bytes);
+  value_nb = avb_be64toh(ph->value_num_bytes);
+
+  // TODO: validate key_nb and value_nb.
+
+  if (p[sizeof(AvbPropertyDescriptor) + key_nb] != 0) {
+    avb_warning("No terminating NUL byte in key.\n");
+    goto out;
+  }
+
+  if (data->key_size == key_nb) {
+    if (avb_memcmp(p + sizeof(AvbPropertyDescriptor), data->key,
+                   data->key_size) == 0) {
+      data->ret_value =
+          (const char*)(p + sizeof(AvbPropertyDescriptor) + key_nb + 1);
+      data->ret_value_size = value_nb;
+      // Stop iterating.
+      ret = 0;
+      goto out;
+    }
+  }
+
+out:
+  return ret;
+}
+
+const char* avb_property_lookup(const uint8_t* image_data, size_t image_size,
+                                const char* key, size_t key_size,
+                                size_t* out_value_size) {
+  PropertyIteratorData data;
+
+  if (key_size == 0) key_size = avb_strlen(key);
+
+  data.key = key;
+  data.key_size = key_size;
+
+  if (avb_descriptor_foreach(image_data, image_size,
+                             property_lookup_desc_foreach, &data) == 0) {
+    if (out_value_size != NULL) *out_value_size = data.ret_value_size;
+    return data.ret_value;
+  }
+
+  if (out_value_size != NULL) *out_value_size = 0;
+  return NULL;
+}
+
+int avb_property_lookup_uint64(const uint8_t* image_data, size_t image_size,
+                               const char* key, size_t key_size,
+                               uint64_t* out_value) {
+  const char* value;
+  int ret = 0;
+  uint64_t parsed_val;
+  int base;
+  int n;
+
+  value = avb_property_lookup(image_data, image_size, key, key_size, NULL);
+  if (value == NULL) goto out;
+
+  base = 10;
+  if (avb_memcmp(value, "0x", 2) == 0) {
+    base = 16;
+    value += 2;
+  }
+
+  parsed_val = 0;
+  for (n = 0; value[n] != '\0'; n++) {
+    int c = value[n];
+    int digit;
+
+    parsed_val *= base;
+
+    switch (base) {
+      case 10:
+        if (c >= '0' && c <= '9') {
+          digit = c - '0';
+        } else {
+          avb_warning("Invalid digit.\n");
+          goto out;
+        }
+        break;
+
+      case 16:
+        if (c >= '0' && c <= '9') {
+          digit = c - '0';
+        } else if (c >= 'a' && c <= 'f') {
+          digit = c - 'a' + 10;
+        } else if (c >= 'A' && c <= 'F') {
+          digit = c - 'A' + 10;
+        } else {
+          avb_warning("Invalid digit.\n");
+          goto out;
+        }
+        break;
+
+      default:
+        goto out;
+    }
+
+    parsed_val += digit;
+  }
+
+  ret = 1;
+  if (out_value != NULL) *out_value = parsed_val;
+
+out:
+  return ret;
+}
