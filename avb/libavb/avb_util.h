11cf69d486c0df1dc42b97140f5a7cf96711d2b1
David Zeuthen <zeuthen@google.com>
2016-07-27 17:58:40 -0400

    avb: Add common verified boot tools and library.
    This code is originally from the Brillo project but has been adapted for
    use in all of Android. It consists of a tool - avbtool - for working
    with images (e.g. boot.img, system.img). See the README file for how
    it's integrated into the Android build system and how to enable it.
    
    The main job of avbtool is to create vbmeta.img which is the
    top-level object for verified boot. This image is designed to go into
    the vbmeta partition (or, if using A/B, the slot in question
    e.g. vbmeta_a or vbmeta_b) and be of minimal size (for out-of-band
    updates). The vbmeta image is cryptographically signed and contains
    verification data (e.g. cryptographic digests) for verifying boot.img,
    system.img, and other partitions/images.
    
    The vbmeta image can also contain references to other partitions where
    verification data is stored as well as a public key indicating who
    should sign the verification data. This indirection provides
    delegation, that is, it allows a 3rd party to control content on a given
    partition by including the public key said 3rd party is using to sign
    the data with, in vbmeta.img. By design, this authority can be easily
    revoked by simply updating vbmeta.img with new descriptors for the
    partition in question.
    
    Storing signed verification data on other images - for example
    boot.img and system.img - is also done with avbtool.
    
    In addition to avbtool, a library - libavb - is provided. This library
    performs all verification on the device side e.g. it starts by loading
    the vbmeta partition, checks the signature, and then goes on to load
    the boot partition for verification.
    
    The libavb library is intended to be used in both boot loaders and
    inside Android. It has a simple abstraction for system dependencies
    (see libavb/avb_sysdeps.h) as well as operations that the boot loader
    or OS is expected to implement (see libavb/avb_ops.h).
    
    In addition to handling verified boot, libavb will in the future be
    extended to handle A/B selection in a way that can be used in the
    device's fastboot implementation, its boot loader, and its
    boot_control HAL implementation. This will be implemented in a future
    CL.
    
    BUG=29414516
    TEST=Unit tests for avbtool and libavb + unit tests pass.
    
    Change-Id: I69ee86878e21fa718faccfc56eb0b1f40707d847

---
+++ avb/libavb/avb_util.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if !defined(AVB_INSIDE_LIBAVB_H) && !defined(AVB_COMPILATION)
+#error "Never include this file directly, include libavb.h instead."
+#endif
+
+#ifndef AVB_UTIL_H_
+#define AVB_UTIL_H_
+
+#include "avb_sysdeps.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Converts a 32-bit unsigned integer from big-endian to host byte order. */
+uint32_t avb_be32toh(uint32_t in) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Converts a 64-bit unsigned integer from big-endian to host byte order. */
+uint64_t avb_be64toh(uint64_t in) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Compare |n| bytes starting at |s1| with |s2| and return 0 if they
+ * match, 1 if they don't.  Returns 0 if |n|==0, since no bytes
+ * mismatched.
+ *
+ * Time taken to perform the comparison is only dependent on |n| and
+ * not on the relationship of the match between |s1| and |s2|.
+ *
+ * Note that unlike avb_memcmp(), this only indicates inequality, not
+ * whether |s1| is less than or greater than |s2|.
+ */
+int avb_safe_memcmp(const void* s1, const void* s2,
+                    size_t n) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Adds |value_to_add| to |value| with overflow protection.
+ *
+ * Returns zero if the addition overflows, non-zero otherwise. In
+ * either case, |value| is always modified.
+ */
+int avb_safe_add_to(uint64_t* value,
+                    uint64_t value_to_add) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Adds |a| and |b| with overflow protection, returning the value in
+ * |out_result|.
+ *
+ * It's permissible to pass NULL for |out_result| if you just want to
+ * check that the addition would not overflow.
+ *
+ * Returns zero if the addition overflows, non-zero otherwise.
+ */
+int avb_safe_add(uint64_t* out_result, uint64_t a,
+                 uint64_t b) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Checks if |num_bytes| data at |data| is a valid UTF-8
+ * string. Returns non-zero if valid UTF-8, zero otherwise.
+ */
+int avb_validate_utf8(const uint8_t* data,
+                      size_t num_bytes) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Concatenates |str1| (of |str1_len| bytes) and |str2| (of |str2_len|
+ * bytes) and puts the result in |buf| which holds |buf_size|
+ * bytes. The result is also guaranteed to be NUL terminated. Fail if
+ * there is not enough room in |buf| for the resulting string plus
+ * terminating NUL byte.
+ *
+ * Returns non-zero if the operation succeeds, zero otherwise.
+ */
+int avb_str_concat(char* buf, size_t buf_size, const char* str1,
+                   size_t str1_len, const char* str2, size_t str2_len);
+
+/* Like avb_malloc_() but prints a error using avb_error() if memory
+ * allocation fails.
+ */
+void* avb_malloc(size_t size) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Like avb_malloc() but sets the memory with zeroes. */
+void* avb_calloc(size_t size) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Duplicates a NUL-terminated string. Returns NULL on OOM. */
+char* avb_strdup(const char* str) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Finds the first occurrence of |needle| in the string |haystack|
+ * where both strings are NUL-terminated strings. The terminating NUL
+ * bytes are not compared.
+ *
+ * Returns NULL if not found, otherwise points into |haystack| for the
+ * first occurrence of |needle|.
+ */
+const char* avb_strstr(const char* haystack,
+                       const char* needle) AVB_ATTR_WARN_UNUSED_RESULT;
+
+/* Replaces all occurrences of |search| with |replace| in |str|.
+ *
+ * Returns a newly allocated string or NULL if out of memory.
+ */
+char* avb_replace(const char* str, const char* search,
+                  const char* replace) AVB_ATTR_WARN_UNUSED_RESULT;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AVB_UTIL_H_ */
