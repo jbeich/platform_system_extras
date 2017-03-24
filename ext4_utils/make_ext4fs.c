93f2c855037ebe063614dfca7ce40ed907e27fb2
I-Jui (Ray) Sung <ijsung@google.com>
2017-03-24 12:28:50 -0700

    Fix SIGFPE when given unusually small legnths
    When the given lenth is too small, there is a chance of division-by-zero
    when computing number of inodes needed. This change makes it a more
    graceful error rather than SIGFPE/core-dump.
    
    Bug: http://b/36576677
    Test: run make_ext4fs with various small sizes
    
    Change-Id: Ia7471a841e9059b6e5bc7d4469ec3c8f05efe119

--- ext4_utils/make_ext4fs.c
+++ ext4_utils/make_ext4fs.c
@@ -383,7 +383,10 @@ static u32 compute_inodes_per_group()
 {
 	u32 blocks = DIV_ROUND_UP(info.len, info.block_size);
 	u32 block_groups = DIV_ROUND_UP(blocks, info.blocks_per_group);
-	u32 inodes = DIV_ROUND_UP(info.inodes, block_groups);
+	u32 inodes;
+	if (!block_groups)
+		error("too few blocks");
+	inodes = DIV_ROUND_UP(info.inodes, block_groups);
 	inodes = EXT4_ALIGN(inodes, (info.block_size / info.inode_size));
 
 	/* After properly rounding up the number of inodes/group,
