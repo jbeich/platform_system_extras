e31f07134537d72a9b74e8baad9d28caaa6205f3
Yabin Cui <yabinc@google.com>
2017-06-02 15:21:50 -0700

    simpleperf: create libsimpleperf_record library.
    Add interface to control perf counters.
    
    Bug: http://b/35725972
    Test: run simpleperf_record_test.
    Change-Id: I2a9979f807253c60a9db6304454934034161c854

---
+++ simpleperf/include/simpleperf.h
@@ -0,0 +1,81 @@
+#ifndef _SIMPLEPERF_H
+#define _SIMPLEPERF_H
+
+#include <sys/types.h>
+
+#include <string>
+#include <vector>
+
+#ifndef SIMPLEPERF_EXPORT
+#define SIMPLEPERF_EXPORT
+#endif
+
+namespace simpleperf {
+
+std::vector<std::string> GetAllEvents() SIMPLEPERF_EXPORT;
+bool IsEventSupported(const std::string& name) SIMPLEPERF_EXPORT;
+
+struct Counter {
+  std::string event;
+  uint64_t value;
+  // If there is not enough hardware counters, kernel will share counters between events.
+  // time_enabled_in_ns is the period when counting is enabled, and time_running_in_ns is
+  // the period when counting really happens in hardware.
+  uint64_t time_enabled_in_ns;
+  uint64_t time_running_in_ns;
+};
+
+enum PerfEventSetType {
+  kPerfForCounting,
+  kPerfForRecording,
+};
+
+// PerfEventSet can be used to count perf events or record perf events in perf.data.
+// To count perf events, you can do as follows:
+//  1. Create PerfEventSet instance.
+//  2. Select perf events to count. You can add more than one events.
+//  3. Set monitored targets.
+//  4. Start/stop/read counters when needed.
+// An example is as below:
+//    PerfEventSet* perf = PerfEventSet::CreateInstance(PerfEventSetType::kPerfForCounting);
+//    perf->AddEvent("cpu-cycles");
+//    perf->AddEvent("instructions");
+//    perf->MonitorCurrentProcess();
+//    perf->StartCounters();
+//    perf->StopCounters();
+//    perf->ReadCounters(&counters);
+//
+// PerfEventSet is not thread-safe. To access it from different threads, please protect
+// it under locks.
+class SIMPLEPERF_EXPORT PerfEventSet {
+ public:
+  static PerfEventSet* CreateInstance(PerfEventSetType type);
+  virtual ~PerfEventSet() {}
+
+  // Add event in the set. All valid events are returned by GetAllEvents().
+  // To only monitor events happen in user space, add :u suffix, like cpu-cycles:u.
+  virtual bool AddEvent(const std::string& name);
+
+  // Set monitored target. You can only monitor threads in current process.
+  virtual bool MonitorCurrentProcess();
+  virtual bool MonitorCurrentThread();
+  virtual bool MonitorThreadsInCurrentProcess(const std::vector<int>& threads);
+
+  // Counting interface:
+  // Start counters. When the PerfEventSet instance is created, the counters are stopped.
+  virtual bool StartCounters();
+  // Stop counters. The values of the counters will not change until the next StartCounters().
+  virtual bool StopCounters();
+  // Read counter values. There is a value for each event. You don't need to stop counters before
+  // reading them. The counter values are the accumulated value from the first StartCounters().
+  virtual bool ReadCounters(std::vector<Counter>* counters);
+
+ protected:
+  PerfEventSet() {}
+};
+
+}  // namespace simpleperf
+
+#undef SIMPLEPERF_EXPORT
+
+#endif  // _SIMPLEPERF_H
