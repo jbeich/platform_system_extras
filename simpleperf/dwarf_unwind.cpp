0fd2d92ea0881c6840e8fe4bc3c6c8c9c36c9277
Christopher Ferris <cferris@google.com>
2017-12-14 15:53:37 -0800

    Change to new libbacktrace offline functions.
    Removes references to libunwind.
    
    Bug: 65682279
    
    Test: All unit tests pass.
    Change-Id: I303b4beab8270ed2097cd17d91253c321cdc97f1

--- simpleperf/dwarf_unwind.cpp
+++ simpleperf/dwarf_unwind.cpp
@@ -133,15 +133,26 @@ bool UnwindCallChain(int abi, const ThreadEntry& thread, const RegSet& regs, con
     bt_map.name = map->dso->GetDebugFilePath();
     bt_map.flags = PROT_READ | PROT_EXEC;
   }
-  std::unique_ptr<BacktraceMap> backtrace_map(BacktraceMap::Create(thread.pid, bt_maps));
 
   backtrace_stackinfo_t stack_info;
   stack_info.start = stack_addr;
   stack_info.end = stack_addr + stack_size;
   stack_info.data = reinterpret_cast<const uint8_t*>(stack);
 
+  Backtrace::ArchEnum backtrace_arch;
+  if (arch == ARCH_ARM) {
+    backtrace_arch = Backtrace::ARCH_ARM;
+  } else if (arch == ARCH_ARM64) {
+    backtrace_arch = Backtrace::ARCH_ARM64;
+  } else if (arch == ARCH_X86_32) {
+    backtrace_arch = Backtrace::ARCH_X86;
+  } else if (arch == ARCH_X86_64) {
+    backtrace_arch = Backtrace::ARCH_X86_64;
+  } else {
+    abort();
+  }
   std::unique_ptr<Backtrace> backtrace(
-      Backtrace::CreateOffline(thread.pid, thread.tid, backtrace_map.get(), stack_info, true));
+      Backtrace::CreateOffline(backtrace_arch, thread.pid, thread.tid, bt_maps, stack_info));
   ucontext_t ucontext = BuildUContextFromRegs(regs);
   if (backtrace->Unwind(0, &ucontext)) {
     for (auto it = backtrace->begin(); it != backtrace->end(); ++it) {
