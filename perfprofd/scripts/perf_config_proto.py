59c0393d197656ceecfb453306c7be2d07939595
Andreas Gampe <agampe@google.com>
2017-12-27 10:57:34 -0800

    Perfprofd: Add startProfilingProtobuf
    Add a binder interface to start profiling with a protobuf detailing
    the configuration. Some light internal refactoring to reuse the
    profiling driver.
    
    Test: m
    Test: adb shell 'cmd perfprofd startProfilingProto - < /data/local/tmp/test.proto'
    Change-Id: I94bb220124d7200d75890bbeb7a5d153d959e02f

---
+++ perfprofd/scripts/perf_config_proto.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Test converter of a Config proto.
+
+import perfprofd_config_pb2
+
+import sys
+
+config = perfprofd_config_pb2.ProfilingConfig()
+
+config.collection_interval_in_s = 10
+config.sample_duration_in_s = 5
+config.main_loop_iterations = 1
+
+f = open(sys.argv[1], "wb")
+f.write(config.SerializeToString())
+f.close()
