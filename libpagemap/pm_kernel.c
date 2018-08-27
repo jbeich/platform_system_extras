125a0ee7bb130b72c2616477c075bd20e7bc8f1f
Sandeep Patil <sspatil@google.com>
2018-08-27 13:57:02 -0700

    Remove Wno-unused-parameter from default and fix resulting warnings.
    Modules fixed were ksmutils, libpagemap, ext4_utils.
    This also results in pm_kernel_pids() to pm_get_pids() API change
    inside libpagemap to accurately represent what the API does.
    
    Bug: 113035007
    Test: make -j48 libpagemap procmem procrank librank ksminfo \
          memtrack_test
    
    Change-Id: I561d7ef0a8d6ea644d8bf046c72590a8981fddb4
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- libpagemap/pm_kernel.c
+++ libpagemap/pm_kernel.c
@@ -74,61 +74,6 @@ int pm_kernel_init_page_idle(pm_kernel_t* ker) {
     return 0;
 }
 
-#define INIT_PIDS 20
-int pm_kernel_pids(pm_kernel_t *ker, pid_t **pids_out, size_t *len) {
-    DIR *proc;
-    struct dirent *dir;
-    pid_t pid, *pids, *new_pids;
-    size_t pids_count, pids_size;
-    int error;
-
-    proc = opendir("/proc");
-    if (!proc)
-        return -errno;
-
-    pids = malloc(INIT_PIDS * sizeof(pid_t));
-    if (!pids) {
-        closedir(proc);
-        return -errno;
-    }
-    pids_count = 0; pids_size = INIT_PIDS;
-
-    while ((dir = readdir(proc))) {
-        if (sscanf(dir->d_name, "%d", &pid) < 1)
-            continue;
-
-        if (pids_count >= pids_size) {
-            new_pids = realloc(pids, 2 * pids_size * sizeof(pid_t));
-            if (!new_pids) {
-                error = -errno;
-                free(pids);
-                closedir(proc);
-                return error;
-            }
-            pids = new_pids;
-            pids_size = 2 * pids_size;
-        }
-
-        pids[pids_count] = pid;
-
-        pids_count++;
-    }
-
-    closedir(proc);
-    
-    new_pids = realloc(pids, pids_count * sizeof(pid_t));
-    if (!new_pids) {
-        error = -errno;
-        free(pids);
-        return error;
-    }
-
-    *pids_out = new_pids;
-    *len = pids_count;
-
-    return 0;
-}
-
 int pm_kernel_count(pm_kernel_t *ker, uint64_t pfn, uint64_t *count_out) {
     off64_t off;
 
