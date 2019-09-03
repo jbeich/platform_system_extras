f5f03c1564b1cbcda2dd2a0a424cb0486bccb61b
Victor Hsieh <victorhsieh@google.com>
2019-09-03 16:05:29 -0700

    fsverity_init: load sideloaded keys from keystore
    Test: Add X.509 DER cert files to keystore, see the key in /proc/keys
          after reboot
    Bug: 112038744
    
    Change-Id: Ie17a2a61385331a5195257a6b69aff6af9d59c06

--- fsverity_init/fsverity_init.cpp
+++ fsverity_init/fsverity_init.cpp
@@ -28,6 +28,17 @@
 #include <android-base/strings.h>
 #include <log/log.h>
 #include <logwrap/logwrap.h>
+#include <keystore/keystore_client.h>
+#include <keystore/keystore_client_impl.h>
+#include <keystore/keystore_get.h>
+
+constexpr char KEY_PREFIX[] = "fsv_";
+constexpr int KEY_UID = 1075;
+
+std::unique_ptr<keystore::KeystoreClient> CreateKeystoreInstance() {
+  return std::unique_ptr<keystore::KeystoreClient>(
+      static_cast<keystore::KeystoreClient*>(new keystore::KeystoreClientImpl));
+}
 
 void SetRequireSignature() {
   if (!android::base::WriteStringToFile("1", "/proc/sys/fs/verity/require_signatures")) {
@@ -69,6 +80,25 @@ void LoadKeyFromVerifiedPartitions() {
   }
 }
 
+void LoadKeysFromKeystore() {
+  auto client = CreateKeystoreInstance();
+
+  std::vector<std::string> aliases;
+  if (client == nullptr || !client->listKeysOfUid(KEY_PREFIX, KEY_UID, &aliases)) {
+    ALOGE("Failed to list key");
+    return;
+  }
+
+  // Always try to load all keys even if some fails to load. The rest may still
+  // be important to have.
+  for (auto &alias : aliases) {
+    auto blob = client->getKey(alias, KEY_UID);
+    if (!LoadKeyToKeyring(true, reinterpret_cast<char*>(blob->data()), blob->size())) {
+      ALOGE("Failed to load key %s from keyring", alias.c_str());
+    }
+  }
+}
+
 void RestrictKeyring() {
   const char* const args[] = {
     "/system/bin/mini-keyctl", "restrict_keyring", ".fs-verity",
@@ -80,6 +110,7 @@ void RestrictKeyring() {
 int main(int /*argc*/, const char** /*argv*/) {
   SetRequireSignature();
   LoadKeyFromVerifiedPartitions();
+  LoadKeysFromKeystore();
   if (!android::base::GetBoolProperty("ro.debuggable", false)) {
     RestrictKeyring();
   }
