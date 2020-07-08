34a8b3aed8c8dbc8c5d8962642258272dbfec4a1
David Anderson <dvander@google.com>
2020-07-08 13:39:01 -0700

    ext4_utils: Don't #define error in a major include.
    Including ext4_utils.h breaks in some files because it defines common
    identifiers (like "error"). Move these into the only .cpp file that uses
    them.
    
    Bug: N/A
    Test: manual test
    Change-Id: I4605b560d09ec46c24a2c42a463f563cfe438356

--- ext4_utils/ext4_utils.cpp
+++ ext4_utils/ext4_utils.cpp
@@ -36,6 +36,12 @@
 #include <sys/disk.h>
 #endif
 
+#define warn(fmt, args...) do { fprintf(stderr, "warning: %s: " fmt "\n", __func__, ## args); } while (0)
+#define error(fmt, args...) do { fprintf(stderr, "error: %s: " fmt "\n", __func__, ## args); if (!force) longjmp(setjmp_env, EXIT_FAILURE); } while (0)
+#define error_errno(s, args...) error(s ": %s", ##args, strerror(errno))
+#define critical_error(fmt, args...) do { fprintf(stderr, "critical error: %s: " fmt "\n", __func__, ## args); longjmp(setjmp_env, EXIT_FAILURE); } while (0)
+#define critical_error_errno(s, args...) critical_error(s ": %s", ##args, strerror(errno))
+
 int force = 0;
 struct fs_info info;
 struct fs_aux_info aux_info;
