b4ceebdae36a1e3ccfee23b76edafd3c919e52b8
Thiébaud Weksteen <tweek@google.com>
2020-10-26 13:37:53 +0100

    ioshark: format files
    Format *.c according to the new .clang-format. The following command
    was used to generate this change:
    
      $ find . \( -name \*.c -o -name \*.h \) -exec clang-format \
        --style=file -i {} \;
    
    NO_TYPO_CHECK=Automated change for formatting. Avoiding any other change to ease the review process.
    Test: mm
    Bug: 171699326
    Change-Id: I82581d8ead443592e23f04abdb3d57148b974d0a

--- ioshark/compile_ioshark.h
+++ ioshark/compile_ioshark.h
@@ -14,68 +14,57 @@
  * limitations under the License.
  */
 
-#define FILE_DB_HASHSIZE	8192
+#define FILE_DB_HASHSIZE 8192
 
 struct files_db_s {
-	char *filename;
-	int fileno;
-	struct files_db_s *next;
-	size_t	size;
-	int	global_filename_ix;
+    char* filename;
+    int fileno;
+    struct files_db_s* next;
+    size_t size;
+    int global_filename_ix;
 };
 
 /* Lifted from Wikipedia Jenkins Hash function page */
-static inline u_int32_t
-jenkins_one_at_a_time_hash(char *key, size_t len)
-{
-	u_int32_t hash, i;
+static inline u_int32_t jenkins_one_at_a_time_hash(char* key, size_t len) {
+    u_int32_t hash, i;
 
-	for(hash = i = 0; i < len; ++i) {
-		hash += key[i];
-		hash += (hash << 10);
-		hash ^= (hash >> 6);
-	}
-	hash += (hash << 3);
-	hash ^= (hash >> 11);
-	hash += (hash << 15);
-	return hash;
+    for (hash = i = 0; i < len; ++i) {
+        hash += key[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash;
 }
 
-static inline void
-files_db_update_size(void *node, u_int64_t new_size)
-{
-	struct files_db_s *db_node = (struct files_db_s *)node;
+static inline void files_db_update_size(void* node, u_int64_t new_size) {
+    struct files_db_s* db_node = (struct files_db_s*)node;
 
-	if (db_node->size < new_size)
-		db_node->size = new_size;
+    if (db_node->size < new_size) db_node->size = new_size;
 }
 
-static inline void
-files_db_add_to_size(void *node, u_int64_t size_incr)
-{
-	((struct files_db_s *)node)->size += size_incr;
+static inline void files_db_add_to_size(void* node, u_int64_t size_incr) {
+    ((struct files_db_s*)node)->size += size_incr;
 }
 
-static inline int
-files_db_get_fileno(void *node)
-{
-	return (((struct files_db_s *)node)->fileno);
+static inline int files_db_get_fileno(void* node) {
+    return (((struct files_db_s*)node)->fileno);
 }
 
-static inline char *
-files_db_get_filename(void *node)
-{
-	return (((struct files_db_s *)node)->filename);
+static inline char* files_db_get_filename(void* node) {
+    return (((struct files_db_s*)node)->filename);
 }
 
-void *files_db_create_handle(void);
-void files_db_write_objects(FILE *fp);
-void *files_db_add(char *filename);
-void *files_db_lookup(char *filename);
+void* files_db_create_handle(void);
+void files_db_write_objects(FILE* fp);
+void* files_db_add(char* filename);
+void* files_db_lookup(char* filename);
 int files_db_get_total_obj(void);
 void init_filename_cache(void);
 void store_filename_cache(void);
 
-int ioshark_write_header(FILE *fp, struct ioshark_header *header);
-int ioshark_write_file_state(FILE *fp, struct ioshark_file_state *state);
-int ioshark_write_file_op(FILE *fp, struct ioshark_file_operation *file_op);
+int ioshark_write_header(FILE* fp, struct ioshark_header* header);
+int ioshark_write_file_state(FILE* fp, struct ioshark_file_state* state);
+int ioshark_write_file_op(FILE* fp, struct ioshark_file_operation* file_op);
--- ioshark/ioshark.h
+++ ioshark/ioshark.h
@@ -29,10 +29,10 @@
  * and the total number of IO operations.
  */
 struct ioshark_header {
-#define IOSHARK_VERSION			2
-	u_int64_t	version;
-	u_int64_t	num_files;
-	u_int64_t	num_io_operations;
+#define IOSHARK_VERSION 2
+    u_int64_t version;
+    u_int64_t num_files;
+    u_int64_t num_io_operations;
 };
 
 /*
@@ -42,88 +42,87 @@ struct ioshark_header {
  * Before the tests starts, these files are pre-created.
  */
 struct ioshark_file_state {
-	u_int64_t	fileno;	/* 1..num_files, with files name ioshark.<fileno> */
-	u_int64_t	size;
-	u_int64_t	global_filename_ix;
+    u_int64_t fileno; /* 1..num_files, with files name ioshark.<fileno> */
+    u_int64_t size;
+    u_int64_t global_filename_ix;
 };
 
 enum file_op {
-	IOSHARK_LSEEK = 0,
-	IOSHARK_LLSEEK,
-	IOSHARK_PREAD64,
-	IOSHARK_PWRITE64,
-	IOSHARK_READ,
-	IOSHARK_WRITE,
-	IOSHARK_MMAP,
-	IOSHARK_MMAP2,
-	IOSHARK_OPEN,
-	IOSHARK_FSYNC,
-	IOSHARK_FDATASYNC,
-	IOSHARK_CLOSE,
-	IOSHARK_MAPPED_PREAD,
-	IOSHARK_MAPPED_PWRITE,
-	IOSHARK_MAX_FILE_OP
+    IOSHARK_LSEEK = 0,
+    IOSHARK_LLSEEK,
+    IOSHARK_PREAD64,
+    IOSHARK_PWRITE64,
+    IOSHARK_READ,
+    IOSHARK_WRITE,
+    IOSHARK_MMAP,
+    IOSHARK_MMAP2,
+    IOSHARK_OPEN,
+    IOSHARK_FSYNC,
+    IOSHARK_FDATASYNC,
+    IOSHARK_CLOSE,
+    IOSHARK_MAPPED_PREAD,
+    IOSHARK_MAPPED_PWRITE,
+    IOSHARK_MAX_FILE_OP
 };
 
 /* mmap prot flags */
-#define IOSHARK_PROT_READ	0x1
-#define IOSHARK_PROT_WRITE	0x2
+#define IOSHARK_PROT_READ 0x1
+#define IOSHARK_PROT_WRITE 0x2
 
 /*
  * Next we have the table of IO operations to perform. Each
  * IO operation is described by this entry.
  */
 struct ioshark_file_operation {
-	/* delta us between previous file op and this */
-	u_int64_t			delta_us;
-#define ioshark_io_op			op_union.file_op_u
-	union {
-		enum file_op		file_op_u;
-		u_int32_t		enum_size;
-	} op_union;
-	u_int64_t			fileno;
-	union {
-		struct lseek_args {
-#define lseek_offset	u.lseek_a.offset
-#define lseek_action	u.lseek_a.action
-			u_int64_t	offset;
-			u_int32_t	action;
-		} lseek_a;
-		struct prw_args {
-#define prw_offset	u.prw_a.offset
-#define prw_len		u.prw_a.len
-			u_int64_t	offset;
-			u_int64_t	len;
-		} prw_a;
-#define rw_len		u.rw_a.len
-		struct rw_args {
-			u_int64_t	len;
-		} rw_a;
-#define mmap_offset	u.mmap_a.offset
-#define mmap_len	u.mmap_a.len
-#define mmap_prot	u.mmap_a.prot
-		struct mmap_args {
-			u_int64_t	offset;
-			u_int64_t	len;
-			u_int32_t	prot;
-	} mmap_a;
-#define open_flags	u.open_a.flags
-#define open_mode	u.open_a.mode
-		struct open_args {
-			u_int32_t	flags;
-			u_int32_t	mode;
-		} open_a;
-	} u;
+    /* delta us between previous file op and this */
+    u_int64_t delta_us;
+#define ioshark_io_op op_union.file_op_u
+    union {
+        enum file_op file_op_u;
+        u_int32_t enum_size;
+    } op_union;
+    u_int64_t fileno;
+    union {
+        struct lseek_args {
+#define lseek_offset u.lseek_a.offset
+#define lseek_action u.lseek_a.action
+            u_int64_t offset;
+            u_int32_t action;
+        } lseek_a;
+        struct prw_args {
+#define prw_offset u.prw_a.offset
+#define prw_len u.prw_a.len
+            u_int64_t offset;
+            u_int64_t len;
+        } prw_a;
+#define rw_len u.rw_a.len
+        struct rw_args {
+            u_int64_t len;
+        } rw_a;
+#define mmap_offset u.mmap_a.offset
+#define mmap_len u.mmap_a.len
+#define mmap_prot u.mmap_a.prot
+        struct mmap_args {
+            u_int64_t offset;
+            u_int64_t len;
+            u_int32_t prot;
+        } mmap_a;
+#define open_flags u.open_a.flags
+#define open_mode u.open_a.mode
+        struct open_args {
+            u_int32_t flags;
+            u_int32_t mode;
+        } open_a;
+    } u;
 };
 
-#define MAX_IOSHARK_PATHLEN	512
+#define MAX_IOSHARK_PATHLEN 512
 
 /*
  * Global table of all fileames
  */
-struct ioshark_filename_struct
-{
-	char path[MAX_IOSHARK_PATHLEN];
+struct ioshark_filename_struct {
+    char path[MAX_IOSHARK_PATHLEN];
 };
 
 #pragma pack(pop)
