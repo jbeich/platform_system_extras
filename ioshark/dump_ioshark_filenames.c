51aeca4d2c6a00bfdea0bc79787ddcbf572aa2eb
Thiébaud Weksteen <tweek@google.com>
2020-10-26 19:25:19 +0100

    ioshark: format files
    Format *.c according to the new .clang-format. The following command
    was used to generate this change:
    
      $ find . \( -name \*.c -o -name \*.h \) -exec clang-format \
        --style=file -i {} \;
    
    Test: mm
    Bug: 171699326
    Change-Id: I93cb461a380a7d32e51e4e2032fa3468c9f633f3

--- ioshark/dump_ioshark_filenames.c
+++ ioshark/dump_ioshark_filenames.c
@@ -13,68 +13,58 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <ctype.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/errno.h>
-#include <fcntl.h>
-#include <ctype.h>
 #include "ioshark.h"
 
 /*
  * Real simple utility that just extracts and dumps the IOshark filenames
  * one per line from the ioshark_filenames file. Useful in debugging.
  */
-int
-main(int argc __attribute__((unused)), char **argv)
-{
-	char *progname;
-	static FILE *filename_cache_fp;
-	struct stat st;
-	struct ioshark_filename_struct *filename_cache;
-	int filename_cache_num_entries;
-	size_t filename_cache_size;
-	int i;
+int main(int argc __attribute__((unused)), char** argv) {
+    char* progname;
+    static FILE* filename_cache_fp;
+    struct stat st;
+    struct ioshark_filename_struct* filename_cache;
+    int filename_cache_num_entries;
+    size_t filename_cache_size;
+    int i;
 
-	progname = argv[0];
-	if (stat("ioshark_filenames", &st) < 0) {
-		fprintf(stderr, "%s Can't stat ioshark_filenames file\n",
-			progname);
-			exit(EXIT_FAILURE);
-	}
-	filename_cache_num_entries = st.st_size /
-		sizeof(struct ioshark_filename_struct);
-	filename_cache_fp = fopen("ioshark_filenames", "r");
-	if (filename_cache_fp == NULL) {
-		fprintf(stderr, "%s Cannot open ioshark_filenames file\n",
-			progname);
-		exit(EXIT_FAILURE);
-	}
-	/* Preallocate a fixed size of entries */
-	filename_cache_size = filename_cache_num_entries + 1024;
-	filename_cache = calloc(filename_cache_size,
-				sizeof(struct ioshark_filename_struct));
-	if (filename_cache == NULL) {
-		fprintf(stderr, "%s Can't allocate memory - this is fatal\n",
-			__func__);
-		exit(EXIT_FAILURE);
-	}
-	if (fread(filename_cache,
-		  sizeof(struct ioshark_filename_struct),
-		  filename_cache_num_entries,
-		  filename_cache_fp) != (size_t)filename_cache_num_entries) {
-		fprintf(stderr, "%s Can't read ioshark_filenames file\n",
-			progname);
-		exit(EXIT_FAILURE);
-	}
-       for (i = 0 ; i < filename_cache_num_entries ; i++) {
-	       printf("%s\n", filename_cache[i].path);
-       }
-       free(filename_cache);
-       fclose(filename_cache_fp);
+    progname = argv[0];
+    if (stat("ioshark_filenames", &st) < 0) {
+        fprintf(stderr, "%s Can't stat ioshark_filenames file\n", progname);
+        exit(EXIT_FAILURE);
+    }
+    filename_cache_num_entries = st.st_size / sizeof(struct ioshark_filename_struct);
+    filename_cache_fp = fopen("ioshark_filenames", "r");
+    if (filename_cache_fp == NULL) {
+        fprintf(stderr, "%s Cannot open ioshark_filenames file\n", progname);
+        exit(EXIT_FAILURE);
+    }
+    /* Preallocate a fixed size of entries */
+    filename_cache_size = filename_cache_num_entries + 1024;
+    filename_cache = calloc(filename_cache_size, sizeof(struct ioshark_filename_struct));
+    if (filename_cache == NULL) {
+        fprintf(stderr, "%s Can't allocate memory - this is fatal\n", __func__);
+        exit(EXIT_FAILURE);
+    }
+    if (fread(filename_cache, sizeof(struct ioshark_filename_struct), filename_cache_num_entries,
+              filename_cache_fp) != (size_t)filename_cache_num_entries) {
+        fprintf(stderr, "%s Can't read ioshark_filenames file\n", progname);
+        exit(EXIT_FAILURE);
+    }
+    for (i = 0; i < filename_cache_num_entries; i++) {
+        printf("%s\n", filename_cache[i].path);
+    }
+    free(filename_cache);
+    fclose(filename_cache_fp);
 }
