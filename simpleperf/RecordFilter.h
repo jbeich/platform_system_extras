b2da7efedeb100c512107565b2e0576f8795d484
Yabin Cui <yabinc@google.com>
2022-01-05 11:50:00 -0800

    simpleperf: use RecordFilter in report cmd.
    Bug: 211814099
    Test: run simpleperf_unit_test
    Change-Id: Idb67eb6b21cda865bd52533308ece944662b849e

--- simpleperf/RecordFilter.h
+++ simpleperf/RecordFilter.h
@@ -31,7 +31,7 @@
 
 namespace simpleperf {
 
-#define RECORD_FILTER_OPTION_HELP_MSG                                                              \
+#define RECORD_FILTER_OPTION_HELP_MSG_FOR_RECORDING                                                \
   "--exclude-pid pid1,pid2,...   Exclude samples for selected processes.\n"                        \
   "--exclude-tid tid1,tid2,...   Exclude samples for selected threads.\n"                          \
   "--exclude-process-name process_name_regex   Exclude samples for processes with name\n"          \
@@ -47,49 +47,22 @@ namespace simpleperf {
   "                                            the regular expression.\n"                          \
   "--include-uid uid1,uid2,...   Include samples for processes belonging to selected uids.\n"
 
-struct RecordFilterCondition {
-  bool used = false;
-  std::set<pid_t> pids;
-  std::set<pid_t> tids;
-  std::vector<std::regex> process_name_regs;
-  std::vector<std::regex> thread_name_regs;
-  std::set<uid_t> uids;
-};
+#define RECORD_FILTER_OPTION_HELP_MSG_FOR_REPORTING                                                \
+  "--exclude-pid pid1,pid2,...   Exclude samples for selected processes.\n"                        \
+  "--exclude-tid tid1,tid2,...   Exclude samples for selected threads.\n"                          \
+  "--exclude-process-name process_name_regex   Exclude samples for processes with name\n"          \
+  "                                            containing the regular expression.\n"               \
+  "--exclude-thread-name thread_name_regex     Exclude samples for threads with name containing\n" \
+  "                                            the regular expression.\n"                          \
+  "--include-pid pid1,pid2,...   Include samples for selected processes.\n"                        \
+  "--include-tid tid1,tid2,...   Include samples for selected threads.\n"                          \
+  "--include-process-name process_name_regex   Include samples for processes with name\n"          \
+  "                                            containing the regular expression.\n"               \
+  "--include-thread-name thread_name_regex     Include samples for threads with name containing\n" \
+  "                                            the regular expression.\n"
 
-// Filter SampleRecords based on the rule below:
-//   out_sample_records = (in_sample_records & ~exclude_conditions) & include_conditions
-//   By default, exclude_conditions = 0, include_conditions = 1.
-class RecordFilter {
- public:
-  RecordFilter(const ThreadTree& thread_tree) : thread_tree_(thread_tree) {}
-  bool ParseOptions(OptionValueMap& options);
-  void AddPids(const std::set<pid_t>& pids, bool exclude);
-  void AddTids(const std::set<pid_t>& tids, bool exclude);
-  void AddProcessNameRegex(const std::string& process_name, bool exclude);
-  void AddThreadNameRegex(const std::string& thread_name, bool exclude);
-  void AddUids(const std::set<uid_t>& uids, bool exclude);
-
-  // Return true if the record passes filter.
-  bool Check(const SampleRecord* r);
-
-  RecordFilterCondition& GetCondition(bool exclude) {
-    return exclude ? exclude_condition_ : include_condition_;
-  }
-  void Clear();
-
- private:
-  bool CheckCondition(const SampleRecord* r, const RecordFilterCondition& condition);
-  bool SearchInRegs(const std::string& s, const std::vector<std::regex>& regs);
-  std::optional<uid_t> GetUidForProcess(pid_t pid);
-
-  const ThreadTree& thread_tree_;
-  RecordFilterCondition exclude_condition_;
-  RecordFilterCondition include_condition_;
-  std::unordered_map<pid_t, std::optional<uid_t>> pid_to_uid_map_;
-};
-
-inline const OptionFormatMap& GetRecordFilterOptionFormats() {
-  static const OptionFormatMap option_formats = {
+inline OptionFormatMap GetRecordFilterOptionFormats(bool for_recording) {
+  OptionFormatMap option_formats = {
       {"--exclude-pid", {OptionValueType::STRING, OptionType::MULTIPLE, AppRunnerType::ALLOWED}},
       {"--exclude-tid", {OptionValueType::STRING, OptionType::MULTIPLE, AppRunnerType::ALLOWED}},
       {"--exclude-process-name",
@@ -103,9 +76,54 @@ inline const OptionFormatMap& GetRecordFilterOptionFormats() {
        {OptionValueType::STRING, OptionType::MULTIPLE, AppRunnerType::ALLOWED}},
       {"--include-thread-name",
        {OptionValueType::STRING, OptionType::MULTIPLE, AppRunnerType::ALLOWED}},
-      {"--include-uid", {OptionValueType::STRING, OptionType::MULTIPLE, AppRunnerType::ALLOWED}},
   };
+  if (for_recording) {
+    option_formats.emplace(
+        "--include-uid",
+        OptionFormat({OptionValueType::STRING, OptionType::MULTIPLE, AppRunnerType::ALLOWED}));
+  }
   return option_formats;
 }
+
+struct RecordFilterCondition {
+  bool used = false;
+  std::set<pid_t> pids;
+  std::set<pid_t> tids;
+  std::vector<std::regex> process_name_regs;
+  std::vector<std::regex> thread_name_regs;
+  std::set<uint32_t> uids;
+};
+
+// Filter SampleRecords based on the rule below:
+//   out_sample_records = (in_sample_records & ~exclude_conditions) & include_conditions
+//   By default, exclude_conditions = 0, include_conditions = 1.
+class RecordFilter {
+ public:
+  RecordFilter(const ThreadTree& thread_tree) : thread_tree_(thread_tree) {}
+  bool ParseOptions(OptionValueMap& options);
+  void AddPids(const std::set<pid_t>& pids, bool exclude);
+  void AddTids(const std::set<pid_t>& tids, bool exclude);
+  void AddProcessNameRegex(const std::string& process_name, bool exclude);
+  void AddThreadNameRegex(const std::string& thread_name, bool exclude);
+  void AddUids(const std::set<uint32_t>& uids, bool exclude);
+
+  // Return true if the record passes filter.
+  bool Check(const SampleRecord* r);
+
+  RecordFilterCondition& GetCondition(bool exclude) {
+    return exclude ? exclude_condition_ : include_condition_;
+  }
+  void Clear();
+
+ private:
+  bool CheckCondition(const SampleRecord* r, const RecordFilterCondition& condition);
+  bool SearchInRegs(const std::string& s, const std::vector<std::regex>& regs);
+  std::optional<uint32_t> GetUidForProcess(pid_t pid);
+
+  const ThreadTree& thread_tree_;
+  RecordFilterCondition exclude_condition_;
+  RecordFilterCondition include_condition_;
+  std::unordered_map<pid_t, std::optional<uint32_t>> pid_to_uid_map_;
+};
 
 }  // namespace simpleperf
