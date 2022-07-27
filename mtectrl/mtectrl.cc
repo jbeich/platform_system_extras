96745801319f406745acaae9b21a16cfe8c960fb
Florian Mayer <fmayer@google.com>
2022-07-27 14:52:29 -0700

    [MTE] Support device_config override in mtectrl.
    Bug: 235129567
    Change-Id: I1afde08dfb6a9c8b8127bf84daebd8f88a865741

--- mtectrl/mtectrl.cc
+++ mtectrl/mtectrl.cc
@@ -21,15 +21,16 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cerr
-        << "Usage: " << argv[0]
-        << " none|memtag|memtag-once|memtag-kernel|memtag-kernel-once[,...]\n";
+  if (argc != 2 && argc != 3) {
+    std::cerr << "Usage: " << argv[0]
+              << " none|memtag|memtag-once|memtag-kernel|memtag-kernel-once[,.."
+                 ".] [default|force_off]\n";
     return 1;
   }
   std::string value = argv[1];
   misc_memtag_message m = {.version = MISC_MEMTAG_MESSAGE_VERSION,
                            .magic = MISC_MEMTAG_MAGIC_HEADER};
+  bool valid_value = true;
   for (const auto& field : android::base::Split(value, ",")) {
     if (field == "memtag") {
       m.memtag_mode |= MISC_MEMTAG_MODE_MEMTAG;
@@ -43,16 +44,52 @@ int main(int argc, char** argv) {
       m.memtag_mode |= MISC_MEMTAG_MODE_MEMTAG_OFF;
     } else if (field != "none") {
       LOG(ERROR) << "Unknown value for arm64.memtag.bootctl: " << field;
-      return 1;
+      valid_value = false;
     }
+  }
+  bool valid_override = true;
+  std::string override_value;
+  if (argc == 3) {
+    override_value = argv[2];
+  }
+  if (override_value == "force_off") {
+    // If the force_off override is active, only allow MEMTAG_MODE_MEMTAG_ONCE.
+    m.memtag_mode |= MISC_MEMTAG_MODE_MEMTAG_OFF;
+    m.memtag_mode &= ~MISC_MEMTAG_MODE_MEMTAG;
+  } else if (!override_value.empty() && override_value != "default") {
+    LOG(ERROR)
+        << "Unknown value for "
+           "persist.device_config.memory_safety_native.bootloader_override: "
+        << override_value;
+    valid_override = false;
+  }
+  if (!valid_value && !valid_override) {
+    return 1;
   }
   std::string err;
   if (!WriteMiscMemtagMessage(m, &err)) {
-    LOG(ERROR) << "Failed to apply arm64.memtag.bootctl: " << value << ". "
-               << err;
+    LOG(ERROR)
+        << "Failed to apply arm64.memtag.bootctl: " << value << ", "
+        << "persist.device_config.memory_safety_native.bootloader_override: "
+        << override_value << err;
     return 1;
   } else {
-    LOG(INFO) << "Applied arm64.memtag.bootctl: " << value;
+    const char* parse_error = "";
+    const char* verb = "Applied";
+    if (!valid_value) {
+      parse_error = " (invalid arm64.memtag.bootctl)";
+      verb = "Partially applied";
+    } else if (!valid_override) {
+      // else if because we bail out if both are false above.
+      parse_error =
+          " (invalid "
+          "persist.device_config.memory_safety_native.bootloader_override)";
+      verb = "Partially applied";
+    }
+    LOG(INFO)
+        << verb << " arm64.memtag.bootctl: " << value << ", "
+        << "persist.device_config.memory_safety_native.bootloader_override: "
+        << override_value << parse_error;
     return 0;
   }
 }
