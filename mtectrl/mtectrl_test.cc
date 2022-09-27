3e8ba52f0e7856a7d77b94731e023c5670c3b2cb
Florian Mayer <fmayer@google.com>
2022-09-26 19:30:45 -0700

    [MTE] add test for mtectrl
    Test: atest --no-bazel-mode mtectrl_test
    Change-Id: I9f6920a1154d33b58fa3f62f809f54dce1e4448d

---
+++ mtectrl/mtectrl_test.cc
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+
+namespace {
+using ::android::base::GetProperty;
+using ::testing::StartsWith;
+
+int mtectrl(const char* arg) {
+  std::string cmd = "mtectrl -t /data/local/tmp/misc_memtag ";
+  cmd += arg;
+  return system(cmd.c_str());
+}
+
+std::string GetMisc() {
+  std::string data;
+  CHECK(android::base::ReadFileToString("/data/local/tmp/misc_memtag", &data, false));
+  return data;
+}
+
+std::string TestProperty() {
+  return GetProperty("arm64.memtag.test_bootctl", "");
+}
+}  // namespace
+
+class MteCtrlTest : public ::testing::Test {
+  void SetUp() override {
+    // Empty fake misc partition.
+    CHECK(android::base::WriteStringToFile("", "/data/local/tmp/misc_memtag", false));
+  }
+  void TearDown() override {
+    CHECK(unlink("/data/local/tmp/misc_memtag") == 0);
+  }
+};
+
+TEST_F(MteCtrlTest, invalid) {
+  EXPECT_NE(mtectrl("memtag-invalid"), 0);
+  EXPECT_NE(mtectrl("memtag override-invalid"), 0);
+}
+
+TEST_F(MteCtrlTest, set_once) {
+  ASSERT_EQ(mtectrl("memtag-once"), 0);
+  EXPECT_THAT(GetMisc(), StartsWith("\x01\x5a\xfe\xfe\x5a\x02"));
+}
+
+TEST_F(MteCtrlTest, set_once_kernel) {
+  ASSERT_EQ(mtectrl("memtag-once,memtag-kernel"), 0);
+  EXPECT_THAT(GetMisc(), StartsWith("\x01\x5a\xfe\xfe\x5a\x06"));
+}
+
+TEST_F(MteCtrlTest, set_memtag) {
+  ASSERT_EQ(mtectrl("memtag"), 0);
+  EXPECT_THAT(GetMisc(), StartsWith("\x01\x5a\xfe\xfe\x5a\x01"));
+}
+
+TEST_F(MteCtrlTest, set_memtag_force_off) {
+  ASSERT_EQ(mtectrl("memtag force_off"), 0);
+  EXPECT_THAT(GetMisc(), StartsWith("\x01\x5a\xfe\xfe\x5a\x10"));
+}
+
+TEST_F(MteCtrlTest, read_memtag) {
+  ASSERT_EQ(mtectrl("memtag"), 0);
+  ASSERT_EQ(mtectrl("-s arm64.memtag.test_bootctl"), 0);
+  EXPECT_EQ(TestProperty(), "memtag");
+}
+
+TEST_F(MteCtrlTest, set_read_memtag) {
+  ASSERT_EQ(mtectrl("-s arm64.memtag.test_bootctl memtag"), 0);
+  EXPECT_EQ(TestProperty(), "memtag");
+}
+
+TEST_F(MteCtrlTest, set_read_force_off) {
+  ASSERT_EQ(mtectrl("-s arm64.memtag.test_bootctl memtag,memtag-once force_off"), 0);
+  EXPECT_EQ(TestProperty(), "memtag-once,memtag-off");
+}
+
+TEST_F(MteCtrlTest, override) {
+  ASSERT_EQ(mtectrl("memtag"), 0);
+  ASSERT_EQ(mtectrl("memtag-once"), 0);
+  EXPECT_THAT(GetMisc(), StartsWith("\x01\x5a\xfe\xfe\x5a\x02"));
+}
