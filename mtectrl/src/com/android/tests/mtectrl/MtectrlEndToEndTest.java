b9ca291f23036f984343a50e9be63c976ada0675
Florian Mayer <fmayer@google.com>
2022-10-26 16:13:54 -0700

    Add end to end test for mtectrl.
    Bug: 255628885
    Change-Id: Ie03b40b8702fa45ea9f8ba2d022975d5cc9024dd

---
+++ mtectrl/src/com/android/tests/mtectrl/MtectrlEndToEndTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tests.mtectrl;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assume.assumeThat;
+
+import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
+import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+// Test the protocol described in
+// https://source.android.com/docs/security/test/memory-safety/bootloader-support.
+// This will reboot the device multiple times, which is perfectly normal.
+
+@RunWith(DeviceJUnit4ClassRunner.class)
+public class MtectrlEndToEndTest extends BaseHostJUnit4Test {
+    private String mPreviousState = null;
+
+    @Before
+    public void setUp() throws Exception {
+        assumeThat(getDevice().getProperty("ro.arm64.memtag.bootctl_supported"), equalTo("1"));
+        mPreviousState = getDevice().getProperty("arm64.memtag.bootctl");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (mPreviousState != null) {
+            getDevice().setProperty("arm64.memtag.bootctl", mPreviousState);
+            java.lang.Thread.sleep(1000); // TODO(b/256042002): Remove once race is fixed.
+            getDevice().reboot();
+        }
+    }
+
+    @Test
+    public void testMemtagOnce() throws Exception {
+        getDevice().setProperty("arm64.memtag.bootctl", "memtag-once");
+        java.lang.Thread.sleep(1000); // TODO(b/256042002): Remove once race is fixed.
+        getDevice().reboot();
+        assertThat(getDevice().pullFileContents("/proc/cpuinfo")).contains("mte");
+        getDevice().reboot();
+        assertThat(getDevice().pullFileContents("/proc/cpuinfo")).doesNotContain("mte");
+    }
+
+    @Test
+    public void testMemtag() throws Exception {
+        getDevice().setProperty("arm64.memtag.bootctl", "memtag");
+        java.lang.Thread.sleep(1000); // TODO(b/256042002): Remove once race is fixed.
+        getDevice().reboot();
+        assertThat(getDevice().pullFileContents("/proc/cpuinfo")).contains("mte");
+        getDevice().reboot();
+        assertThat(getDevice().pullFileContents("/proc/cpuinfo")).contains("mte");
+    }
+}
