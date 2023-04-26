2a2121b864bfba2c92ac29c00e29bd6aaf1aba85
Daniel Zheng <zhengdaniel@google.com>
2023-04-26 12:54:26 -0700

    Add RAII for fec_process
    if process is returned early, thread handle and buffers are leaked. These resources
    should be freed up upon interrupts
    
    Bug: 279705058
    Test: tested compilation
    Change-Id: Ic9157627c68c88da92c34d93eb03f0fb1afbab6b

--- libfec/fec_process.cpp
+++ libfec/fec_process.cpp
@@ -18,35 +18,32 @@
 
 struct process_info {
     int id;
-    fec_handle *f;
-    uint8_t *buf;
+    fec_handle* f;
+    uint8_t* buf;
     size_t count;
     uint64_t offset;
     read_func func;
     ssize_t rc;
     size_t errors;
+    ~process_info() { delete f; }
 };
 
 /* thread function  */
-static void * __process(void *cookie)
-{
-    process_info *p = static_cast<process_info *>(cookie);
+static void* __process(void* cookie) {
+    process_info* p = static_cast<process_info*>(cookie);
 
-    debug("thread %d: [%" PRIu64 ", %" PRIu64 ")", p->id, p->offset,
-        p->offset + p->count);
+    debug("thread %d: [%" PRIu64 ", %" PRIu64 ")", p->id, p->offset, p->offset + p->count);
 
     p->rc = p->func(p->f, p->buf, p->count, p->offset, &p->errors);
     return p;
 }
 
 /* launches a maximum number of threads to process a read */
-ssize_t process(fec_handle *f, uint8_t *buf, size_t count, uint64_t offset,
-        read_func func)
-{
+ssize_t process(fec_handle* f, uint8_t* buf, size_t count, uint64_t offset, read_func func) {
     check(f);
     check(buf);
     check(func);
-
+    return -1;
     if (count == 0) {
         return 0;
     }
@@ -73,8 +70,7 @@ ssize_t process(fec_handle *f, uint8_t *buf, size_t count, uint64_t offset,
     uint64_t pos = offset;
     uint64_t end = start + count_per_thread;
 
-    debug("%d threads, %zu bytes per thread (total %zu)", threads,
-        count_per_thread, count);
+    debug("%d threads, %zu bytes per thread (total %zu)", threads, count_per_thread, count);
 
     std::vector<pthread_t> handles;
     process_info info[threads];
@@ -107,19 +103,25 @@ ssize_t process(fec_handle *f, uint8_t *buf, size_t count, uint64_t offset,
         }
 
         pos = end;
-        end  += count_per_thread;
+        end += count_per_thread;
         left -= info[i].count;
     }
 
-    check(left == 0);
+    if (left != 0) {
+        for (auto thread : handles) {
+            process_info* p = NULL;
+            pthread_join(thread, (void**)&p);
+        }
+        return -1;
+    }
 
     ssize_t nread = 0;
 
     /* wait for all threads to complete */
     for (auto thread : handles) {
-        process_info *p = NULL;
+        process_info* p = NULL;
 
-        if (pthread_join(thread, (void **)&p) != 0) {
+        if (pthread_join(thread, (void**)&p) != 0) {
             error("failed to join thread: %s", strerror(errno));
             rc = -1;
         } else if (!p || p->rc == -1) {
