f3cd88c84a70d7d025dc0c2bca43ddf368f16627
Esteban Barrero-Hernandez <estebanbarrero@google.com>
2024-07-11 21:03:08 +0000

    Implemented argument parsing and command creation in torq.py.
    Implemented command classes in command.py. Implemented command executor classes in
    command_executor.py. Implemented error class in validation_error.py. Created skeleton of unit test
    file in tests/torq_unit_test.py. Also, created Android.pb and OWNERS files.
    
    Test: atest torq_unit_test
    Bug: 352571366
    
    Change-Id: Ieef9053250632ab3e1f479497d6f4a61ec55cbc7

---
+++ torq/tests/torq_unit_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TorqUnitTest(unittest.TestCase):
+
+  def test_parser(self):
+    # TODO(b/352744897): Implement tests
+    pass
+
+
+if __name__ == '__main__':
+  unittest.main()
