83fdefb3641ba609f3092da458e4a618eeaee71e
Esteban Barrero-Hernandez <estebanbarrero@google.com>
2024-08-20 21:06:06 +0000

    Implemented uploading prefetto trace output to perfetto web UI.
    This involved introducing open_ui.py, which contains
    the new HttpHandler class and open_trace method,
    integrating open_trace into command_executor.py, and
    updating tests/command_executor_unit_test.py and
    android.bp accordingly. Finally, minor style improvements
    were made to tests/command_executor_unit_test.py
    
    Test: atest_command_executor_unit_test
    Bug: 355053544
    
    Test: atest_command_executor_unit_test
    
    Change-Id: Iff1b6ddd47e4889107b44226e967c369ad6b267f

--- torq/tests/command_executor_unit_test.py
+++ torq/tests/command_executor_unit_test.py
@@ -16,15 +16,20 @@
 
 import unittest
 import sys
+import command_executor
 from unittest import mock
 from io import StringIO
 from command import ProfilerCommand
 from command_executor import ProfilerCommandExecutor
 from validation_error import ValidationError
+from torq import DEFAULT_OUT_DIR
+import open_ui
 
 MOCK_DEVICE = "mock-device"
 MOCK_ERROR = "mock-error"
 MOCK_CONFIG = "mock-config"
+MOCK_HOST_PATH = "mock-host-path"
+MOCK_VALIDATION_ERROR = ValidationError(MOCK_ERROR, None)
 
 
 class ProfilerCommandExecutorUnitTest(unittest.TestCase):
@@ -34,6 +39,7 @@ class ProfilerCommandExecutorUnitTest(unittest.TestCase):
                                                 "create_config").start()
 
   @mock.patch.object(sys, "stdout", new_callable=StringIO)
+  @mock.patch.object(command_executor, "open_trace", autospec=True)
   @mock.patch.object(ProfilerCommandExecutor, "retrieve_perf_data",
                      autospec=True)
   @mock.patch.object(ProfilerCommandExecutor, "execute_run", autospec=True)
@@ -41,32 +47,52 @@ class ProfilerCommandExecutorUnitTest(unittest.TestCase):
                      autospec=True)
   def test_execute_command_print_current_run_success(self,
       mock_prepare_device_for_run, mock_execute_run, mock_retrieve_perf_data,
-      mock_terminal_output):
+      mock_open_trace,  mock_terminal_output):
     mock_command = mock.create_autospec(ProfilerCommand, instance=True)
     mock_command.runs = 1
     mock_command.use_ui = False
+    mock_command.out_dir = DEFAULT_OUT_DIR
     self.mock_create_config.return_value = MOCK_CONFIG, None
     mock_prepare_device_for_run.return_value = None
     mock_execute_run.side_effect = (lambda profiler_command_executor, device,
         command, config, run: (print("Performing run %s" % run), None)[1])
     mock_retrieve_perf_data.return_value = None
-    command_executor = ProfilerCommandExecutor()
+    mock_open_trace.return_value = None
+    executor = ProfilerCommandExecutor()
 
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
 
     self.assertEqual(error, None)
     self.assertEqual(mock_terminal_output.getvalue().strip(),
                      "Performing run 1")
 
+  @mock.patch.object(ProfilerCommandExecutor, "retrieve_perf_data",
+                     autospec=True)
+  @mock.patch.object(ProfilerCommandExecutor, "execute_run", autospec=True)
+  @mock.patch.object(ProfilerCommandExecutor, "prepare_device_for_run",
+                     autospec=True)
+  def test_execute_command_no_ui_success(self, mock_prepare_device_for_run,
+      mock_execute_run, mock_retrieve_perf_data):
+    mock_command = mock.create_autospec(ProfilerCommand, instance=True)
+    mock_command.runs = 1
+    mock_command.out_dir = DEFAULT_OUT_DIR
+    mock_command.use_ui = False
+    self.mock_create_config.return_value = MOCK_CONFIG, None
+    mock_prepare_device_for_run.return_value = None
+    mock_execute_run.return_value = None
+    mock_retrieve_perf_data.return_value = None
+    executor = ProfilerCommandExecutor()
+
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
+
+    self.assertEqual(error, None)
+
   def test_execute_command_create_config_failure(self):
     mock_command = mock.create_autospec(ProfilerCommand, instance=True)
-    mock_error = ValidationError(MOCK_ERROR, None)
-    self.mock_create_config.return_value = None, mock_error
-    command_executor = ProfilerCommandExecutor()
+    self.mock_create_config.return_value = None, MOCK_VALIDATION_ERROR
+    executor = ProfilerCommandExecutor()
 
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
 
     self.assertNotEqual(error, None)
     self.assertEqual(error.message, MOCK_ERROR)
@@ -75,13 +101,11 @@ class ProfilerCommandExecutorUnitTest(unittest.TestCase):
   @mock.patch.object(ProfilerCommandExecutor, "prepare_device", autospec=True)
   def test_execute_command_prepare_device_failure(self, mock_prepare_device):
     mock_command = mock.create_autospec(ProfilerCommand, instance=True)
-    mock_error = ValidationError(MOCK_ERROR, None)
     self.mock_create_config.return_value = MOCK_CONFIG, None
-    mock_prepare_device.return_value = mock_error
-    command_executor = ProfilerCommandExecutor()
+    mock_prepare_device.return_value = MOCK_VALIDATION_ERROR
+    executor = ProfilerCommandExecutor()
 
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
 
     self.assertNotEqual(error, None)
     self.assertEqual(error.message, MOCK_ERROR)
@@ -93,13 +117,12 @@ class ProfilerCommandExecutorUnitTest(unittest.TestCase):
       mock_prepare_device_for_run):
     mock_command = mock.create_autospec(ProfilerCommand, instance=True)
     mock_command.runs = 1
-    mock_error = ValidationError(MOCK_ERROR, None)
+    mock_command.out_dir = DEFAULT_OUT_DIR
     self.mock_create_config.return_value = MOCK_CONFIG, None
-    mock_prepare_device_for_run.return_value = mock_error
-    command_executor = ProfilerCommandExecutor()
+    mock_prepare_device_for_run.return_value = MOCK_VALIDATION_ERROR
+    executor = ProfilerCommandExecutor()
 
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
 
     self.assertNotEqual(error, None)
     self.assertEqual(error.message, MOCK_ERROR)
@@ -112,14 +135,13 @@ class ProfilerCommandExecutorUnitTest(unittest.TestCase):
       mock_prepare_device_for_run, mock_execute_run):
     mock_command = mock.create_autospec(ProfilerCommand, instance=True)
     mock_command.runs = 1
-    mock_error = ValidationError(MOCK_ERROR, None)
+    mock_command.out_dir = DEFAULT_OUT_DIR
     self.mock_create_config.return_value = MOCK_CONFIG, None
     mock_prepare_device_for_run.return_value = None
-    mock_execute_run.return_value = mock_error
-    command_executor = ProfilerCommandExecutor()
+    mock_execute_run.return_value = MOCK_VALIDATION_ERROR
+    executor = ProfilerCommandExecutor()
 
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
 
     self.assertNotEqual(error, None)
     self.assertEqual(error.message, MOCK_ERROR)
@@ -134,15 +156,14 @@ class ProfilerCommandExecutorUnitTest(unittest.TestCase):
       mock_prepare_device_for_run, mock_execute_run, mock_retrieve_perf_data):
     mock_command = mock.create_autospec(ProfilerCommand, instance=True)
     mock_command.runs = 1
-    mock_error = ValidationError(MOCK_ERROR, None)
+    mock_command.out_dir = DEFAULT_OUT_DIR
     self.mock_create_config.return_value = MOCK_CONFIG, None
     mock_prepare_device_for_run.return_value = None
     mock_execute_run.return_value = None
-    mock_retrieve_perf_data.return_value = mock_error
-    command_executor = ProfilerCommandExecutor()
+    mock_retrieve_perf_data.return_value = MOCK_VALIDATION_ERROR
+    executor = ProfilerCommandExecutor()
 
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
 
     self.assertNotEqual(error, None)
     self.assertEqual(error.message, MOCK_ERROR)
@@ -158,86 +179,15 @@ class ProfilerCommandExecutorUnitTest(unittest.TestCase):
       mock_execute_run, mock_retrieve_perf_data, mock_cleanup):
     mock_command = mock.create_autospec(ProfilerCommand, instance=True)
     mock_command.runs = 1
-    mock_error = ValidationError(MOCK_ERROR, None)
+    mock_command.out_dir = DEFAULT_OUT_DIR
     self.mock_create_config.return_value = MOCK_CONFIG, None
     mock_prepare_device_for_run.return_value = None
     mock_execute_run.return_value = None
     mock_retrieve_perf_data.return_value = None
-    mock_cleanup.return_value = mock_error
-    command_executor = ProfilerCommandExecutor()
+    mock_cleanup.return_value = MOCK_VALIDATION_ERROR
+    executor = ProfilerCommandExecutor()
 
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
-
-    self.assertNotEqual(error, None)
-    self.assertEqual(error.message, MOCK_ERROR)
-    self.assertEqual(error.suggestion, None)
-
-  @mock.patch.object(ProfilerCommandExecutor, "retrieve_perf_data",
-                     autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "execute_run", autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "prepare_device_for_run",
-                     autospec=True)
-  def test_execute_command_no_ui_success(self, mock_prepare_device_for_run,
-      mock_execute_run, mock_retrieve_perf_data):
-    mock_command = mock.create_autospec(ProfilerCommand, instance=True)
-    mock_command.runs = 1
-    mock_command.use_ui = False
-    self.mock_create_config.return_value = MOCK_CONFIG, None
-    mock_prepare_device_for_run.return_value = None
-    mock_execute_run.return_value = None
-    mock_retrieve_perf_data.return_value = None
-    command_executor = ProfilerCommandExecutor()
-
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
-
-    self.assertEqual(error, None)
-
-  @mock.patch.object(ProfilerCommandExecutor, "open_ui", autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "retrieve_perf_data",
-                     autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "execute_run", autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "prepare_device_for_run",
-                     autospec=True)
-  def test_execute_command_use_ui_success(self, mock_prepare_device_for_run,
-      mock_execute_run, mock_retrieve_perf_data, mock_open_ui):
-    mock_command = mock.create_autospec(ProfilerCommand, instance=True)
-    mock_command.runs = 1
-    mock_command.use_ui = True
-    self.mock_create_config.return_value = MOCK_CONFIG, None
-    mock_prepare_device_for_run.return_value = None
-    mock_execute_run.return_value = None
-    mock_retrieve_perf_data.return_value = None
-    mock_open_ui.return_value = None
-    command_executor = ProfilerCommandExecutor()
-
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
-
-    self.assertEqual(error, None)
-
-  @mock.patch.object(ProfilerCommandExecutor, "open_ui", autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "retrieve_perf_data",
-                     autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "execute_run", autospec=True)
-  @mock.patch.object(ProfilerCommandExecutor, "prepare_device_for_run",
-                     autospec=True)
-  def test_execute_command_use_ui_failure(self, mock_prepare_device_for_run,
-      mock_execute_run, mock_retrieve_perf_data, mock_open_ui):
-    mock_command = mock.create_autospec(ProfilerCommand, instance=True)
-    mock_command.runs = 1
-    mock_command.use_ui = True
-    mock_error = ValidationError(MOCK_ERROR, None)
-    self.mock_create_config.return_value = MOCK_CONFIG, None
-    mock_prepare_device_for_run.return_value = None
-    mock_execute_run.return_value = None
-    mock_retrieve_perf_data.return_value = None
-    mock_open_ui.return_value = mock_error
-    command_executor = ProfilerCommandExecutor()
-
-    error = (
-        command_executor.execute_command(mock_command, MOCK_DEVICE))
+    error = executor.execute_command(mock_command, MOCK_DEVICE)
 
     self.assertNotEqual(error, None)
     self.assertEqual(error.message, MOCK_ERROR)
